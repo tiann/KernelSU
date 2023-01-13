177d6dba158abc6a873b4d1e5d9d4e41bebb8331
Ylarod <me@ylarod.cn>
2023-01-13 20:05:52 +0800

    kernel: add prefix for functions in sepolicy (#49)
    * kernel: add prefix for functions in sepolicy
    
    * ci: fix 5.15 compile error

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -31,61 +31,61 @@ void apply_kernelsu_rules()
 	db = &ss->policydb;
 #endif
 
-	permissive(db, KERNEL_SU_DOMAIN);
-	typeattribute(db, KERNEL_SU_DOMAIN, "mlstrustedsubject");
-	typeattribute(db, KERNEL_SU_DOMAIN, "netdomain");
-	typeattribute(db, KERNEL_SU_DOMAIN, "bluetoothdomain");
+	ksu_permissive(db, KERNEL_SU_DOMAIN);
+	ksu_typeattribute(db, KERNEL_SU_DOMAIN, "mlstrustedsubject");
+	ksu_typeattribute(db, KERNEL_SU_DOMAIN, "netdomain");
+	ksu_typeattribute(db, KERNEL_SU_DOMAIN, "bluetoothdomain");
 
 	// allow all!
-	allow(db, KERNEL_SU_DOMAIN, ALL, ALL, ALL);
+	ksu_allow(db, KERNEL_SU_DOMAIN, ALL, ALL, ALL);
 
 	// allow us do any ioctl
 	if (db->policyvers >= POLICYDB_VERSION_XPERMS_IOCTL) {
-		allowxperm(db, KERNEL_SU_DOMAIN, ALL, "blk_file", ALL);
-		allowxperm(db, KERNEL_SU_DOMAIN, ALL, "fifo_file", ALL);
-		allowxperm(db, KERNEL_SU_DOMAIN, ALL, "chr_file", ALL);
+		ksu_allowxperm(db, KERNEL_SU_DOMAIN, ALL, "blk_file", ALL);
+		ksu_allowxperm(db, KERNEL_SU_DOMAIN, ALL, "fifo_file", ALL);
+		ksu_allowxperm(db, KERNEL_SU_DOMAIN, ALL, "chr_file", ALL);
 	}
 
 	// we need to save allowlist in /data/adb
-	allow(db, "kernel", "adb_data_file", "dir", ALL);
-	allow(db, "kernel", "adb_data_file", "file", ALL);
+	ksu_allow(db, "kernel", "adb_data_file", "dir", ALL);
+	ksu_allow(db, "kernel", "adb_data_file", "file", ALL);
 	// we may need to do mount on shell
-	allow(db, "kernel", "shell_data_file", "file", ALL);
+	ksu_allow(db, "kernel", "shell_data_file", "file", ALL);
 
 	// our ksud triggered by init
-	allow(db, "init", "adb_data_file", "file", "execute");
-	allow(db, "init", KERNEL_SU_DOMAIN, ALL, ALL);
+	ksu_allow(db, "init", "adb_data_file", "file", "execute");
+	ksu_allow(db, "init", KERNEL_SU_DOMAIN, ALL, ALL);
 
 	// copied from Magisk rules
 	// suRights
-	allow(db, "servicemanager", KERNEL_SU_DOMAIN, "dir", "search");
-	allow(db, "servicemanager", KERNEL_SU_DOMAIN, "dir", "read");
-	allow(db, "servicemanager", KERNEL_SU_DOMAIN, "file", "open");
-	allow(db, "servicemanager", KERNEL_SU_DOMAIN, "file", "read");
-	allow(db, "servicemanager", KERNEL_SU_DOMAIN, "process", "getattr");
-	allow(db, ALL, KERNEL_SU_DOMAIN, "process", "sigchld");
+	ksu_allow(db, "servicemanager", KERNEL_SU_DOMAIN, "dir", "search");
+	ksu_allow(db, "servicemanager", KERNEL_SU_DOMAIN, "dir", "read");
+	ksu_allow(db, "servicemanager", KERNEL_SU_DOMAIN, "file", "open");
+	ksu_allow(db, "servicemanager", KERNEL_SU_DOMAIN, "file", "read");
+	ksu_allow(db, "servicemanager", KERNEL_SU_DOMAIN, "process", "getattr");
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "process", "sigchld");
 
 	// allowLog
-	allow(db, "logd", KERNEL_SU_DOMAIN, "dir", "search");
-	allow(db, "logd", KERNEL_SU_DOMAIN, "file", "read");
-	allow(db, "logd", KERNEL_SU_DOMAIN, "file", "open");
-	allow(db, "logd", KERNEL_SU_DOMAIN, "file", "getattr");
+	ksu_allow(db, "logd", KERNEL_SU_DOMAIN, "dir", "search");
+	ksu_allow(db, "logd", KERNEL_SU_DOMAIN, "file", "read");
+	ksu_allow(db, "logd", KERNEL_SU_DOMAIN, "file", "open");
+	ksu_allow(db, "logd", KERNEL_SU_DOMAIN, "file", "getattr");
 
 	// dumpsys
-	allow(db, ALL, KERNEL_SU_DOMAIN, "fd", "use");
-	allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "write");
-	allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "read");
-	allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "open");
-	allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "getattr");
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "fd", "use");
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "write");
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "read");
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "open");
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "fifo_file", "getattr");
 
 	// bootctl
-	allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "dir", "search");
-	allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "file", "read");
-	allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "file", "open");
-	allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "process", "getattr");
+	ksu_allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "dir", "search");
+	ksu_allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "file", "read");
+	ksu_allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "file", "open");
+	ksu_allow(db, "hwservicemanager", KERNEL_SU_DOMAIN, "process", "getattr");
 
 	// Allow all binder transactions
-	allow(db, ALL, KERNEL_SU_DOMAIN, "binder", ALL);
+	ksu_allow(db, ALL, KERNEL_SU_DOMAIN, "binder", ALL);
 
 	rcu_read_unlock();
 }
--- kernel/selinux/sepolicy.h
+++ kernel/selinux/sepolicy.h
@@ -6,59 +6,32 @@
 #include <ss/services.h>
 #include <objsec.h>
 
+
 // Operation on types
-bool type(struct policydb* db, const char* name, const char* attr);
-bool attribute(struct policydb* db, const char* name);
-bool permissive(struct policydb* db, const char* type);
-bool enforce(struct policydb* db, const char* type);
-bool typeattribute(struct policydb* db, const char* type, const char* attr);
-bool exists(struct policydb* db, const char* type);
+bool ksu_type(struct policydb* db, const char* name, const char* attr);
+bool ksu_attribute(struct policydb* db, const char* name);
+bool ksu_permissive(struct policydb* db, const char* type);
+bool ksu_enforce(struct policydb* db, const char* type);
+bool ksu_typeattribute(struct policydb* db, const char* type, const char* attr);
+bool ksu_exists(struct policydb* db, const char* type);
 
 // Access vector rules
-bool allow(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
-bool deny(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
-bool auditallow(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
-bool dontaudit(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
+bool ksu_allow(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
+bool ksu_deny(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
+bool ksu_auditallow(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
+bool ksu_dontaudit(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* perm);
 
 // Extended permissions access vector rules
-bool allowxperm(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* range);
-bool auditallowxperm(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* range);
-bool dontauditxperm(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* range);
+bool ksu_allowxperm(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* range);
+bool ksu_auditallowxperm(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* range);
+bool ksu_dontauditxperm(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* range);
 
 // Type rules
-bool type_transition(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* def, const char* obj);
-bool type_change(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* def);
-bool type_member(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* def);
+bool ksu_type_transition(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* def, const char* obj);
+bool ksu_type_change(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* def);
+bool ksu_type_member(struct policydb* db, const char* src, const char* tgt, const char* cls, const char* def);
 
 // File system labeling
-bool genfscon(struct policydb* db, const char* fs_name, const char* path, const char* ctx);
-
-
-//////////////////////////////////////////////////////
-// Internal use
-//////////////////////////////////////////////////////
-
-struct avtab_node* get_avtab_node(struct policydb* db, struct avtab_key *key, struct avtab_extended_perms *xperms);
-
-bool add_rule(struct policydb* db, const char *s, const char *t, const char *c, const char *p, int effect, bool invert);
-void add_rule_raw(struct policydb* db, struct type_datum *src, struct type_datum *tgt, struct class_datum *cls, struct perm_datum *perm, int effect, bool invert);
-
-void add_xperm_rule_raw(struct policydb* db, struct type_datum *src, struct type_datum *tgt,
-        struct class_datum *cls, uint16_t low, uint16_t high, int effect, bool invert);
-bool add_xperm_rule(struct policydb* db, const char *s, const char *t, const char *c, const char *range, int effect, bool invert);
-
-bool add_type_rule(struct policydb* db, const char *s, const char *t, const char *c, const char *d, int effect);
-
-bool add_filename_trans(const char *s, const char *t, const char *c, const char *d, const char *o);
-
-bool add_genfscon(const char *fs_name, const char *path, const char *context);
-
-bool add_type(struct policydb* db, const char *type_name, bool attr);
-
-bool set_type_state(struct policydb* db, const char *type_name, bool permissive);
-
-void add_typeattribute_raw(struct policydb* db, struct type_datum *type, struct type_datum *attr);
-
-bool add_typeattribute(struct policydb* db, const char *type, const char *attr);
+bool ksu_genfscon(struct policydb* db, const char* fs_name, const char* path, const char* ctx);
 
 #endif
