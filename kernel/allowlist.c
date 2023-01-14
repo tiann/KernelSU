4d20cb8200f99d2d7b32ed33d006b24e93619e43
Ylarod <me@ylarod.cn>
2023-01-14 18:56:43 +0800

    allow shell by default

--- kernel/allowlist.c
+++ kernel/allowlist.c
@@ -181,7 +181,16 @@ void do_load_allow_list(struct work_struct *work)
 	fp = filp_open(KERNEL_SU_ALLOWLIST, O_RDONLY, 0);
 
 	if (IS_ERR(fp)) {
+#ifdef CONFIG_KSU_DEBUG
+		int errno = PTR_ERR(fp);
+		if (errno == -ENOENT) {
+			ksu_allow_uid(2000, true); // allow adb shell by default
+		}else{
+			pr_err("load_allow_list open file failed: %d\n", PTR_ERR(fp));
+		}
+#else
 		pr_err("load_allow_list open file failed: %d\n", PTR_ERR(fp));
+#endif
 		return;
 	}
 
