2724c6211ab505b31c4f7fb414026f986725be22
Ylarod <me@ylarod.cn>
2023-01-18 07:59:26 +0800

    kernel: put original group_info (#82)

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -64,6 +64,7 @@ void escape_to_root()
 	current->seccomp.filter = NULL;
 
 	// setgroup to root
+	put_group_info(cred->group_info);
 	cred->group_info = get_group_info(&root_groups);
 
 	setup_selinux();
