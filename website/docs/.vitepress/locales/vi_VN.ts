37d0ad763e677c57e016ee5c153f64bccd576886
Huy Minh <39849246+hmtheboy154@users.noreply.github.com>
2023-01-21 21:10:37 +0700

    Website : vi_VN : Capitalize "guide" in Vietnamese

--- website/docs/.vitepress/locales/vi_VN.ts
+++ website/docs/.vitepress/locales/vi_VN.ts
@@ -35,7 +35,7 @@ export default defineConfig({
 
 function nav() {
   return [
-    { text: 'hướng dẫn', link: '/vi_VN/guide/what-is-kernelsu' },
+    { text: 'Hướng Dẫn', link: '/vi_VN/guide/what-is-kernelsu' },
     { text: 'Github', link: 'https://github.com/tiann/KernelSU' }
   ]
 }
