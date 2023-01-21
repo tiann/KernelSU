a31b7650ab10c2c18a5a86b1b7b3e170055f8802
tiann <twsxtd@gmail.com>
2023-01-21 18:51:01 +0800

    manager: use libsu to getenforce

--- manager/app/src/main/java/me/weishu/kernelsu/ui/util/SELinuxChecker.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/util/SELinuxChecker.kt
@@ -2,31 +2,31 @@ package me.weishu.kernelsu.ui.util
 
 import androidx.compose.ui.res.stringResource
 import androidx.compose.runtime.Composable
+import com.topjohnwu.superuser.Shell
 import me.weishu.kernelsu.R
 
-private const val TAG = "SELinuxChecker"
-
 @Composable
-fun SELinuxStatus(): String {
-    val getSELinuxStatus = Runtime.getRuntime().exec("getenforce")
-    getSELinuxStatus.waitFor()
-    var getSELinuxStatusisSuccessful: Boolean = false
-    getSELinuxStatusisSuccessful = (getSELinuxStatus.exitValue() == 0)
+fun getSELinuxStatus(): String {
+    val shell = Shell.Builder.create()
+        .setFlags(Shell.FLAG_REDIRECT_STDERR)
+        .build("sh")
 
-    val getSELinuxStatusOut = getSELinuxStatus.errorStream.bufferedReader().use { it.readLine() } ?: getSELinuxStatus.inputStream.bufferedReader().use { it.readLine() }
-    val checkSELinuxStatus = if (getSELinuxStatusisSuccessful) {
-        when (getSELinuxStatusOut) {
+    val list = ArrayList<String>()
+    val result = shell.newJob().add("getenforce").to(list, list).exec()
+    val output = result.out.joinToString("\n").trim()
+
+    if (result.isSuccess) {
+        return when (output) {
             "Enforcing" -> stringResource(R.string.selinux_status_enforcing)
             "Permissive" -> stringResource(R.string.selinux_status_permissive)
             "Disabled" -> stringResource(R.string.selinux_status_disabled)
             else -> stringResource(R.string.selinux_status_unknown)
         }
-    } else {
-        if (getSELinuxStatusOut?.endsWith("Permission denied") == true) {
-            stringResource(R.string.selinux_status_enforcing)
-        } else {
-            stringResource(R.string.selinux_status_unknown)
-        }
     }
-    return checkSELinuxStatus
+
+    return if (output.endsWith("Permission denied")) {
+        stringResource(R.string.selinux_status_enforcing)
+    } else {
+        stringResource(R.string.selinux_status_unknown)
+    }
 }
