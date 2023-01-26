8446f07a91617bb1a0c3956ba914facf951f3cde
Ylarod <me@ylarod.cn>
2023-01-26 14:32:52 +0800

    kernel: update debug warning (#121)

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -36,14 +36,13 @@ extern void ksu_enable_ksud();
 int __init kernelsu_init(void)
 {
 #ifdef CONFIG_KSU_DEBUG
-	pr_alert(
-		"*************************************************************\n"
-		"**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **\n"
-		"**                                                         **\n"
-		"**         You are running DEBUG version of KernelSU       **\n"
-		"**                                                         **\n"
-		"**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **\n"
-		"*************************************************************\n");
+	pr_alert("*************************************************************");
+	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+	pr_alert("**                                                         **");
+	pr_alert("**         You are running DEBUG version of KernelSU       **");
+	pr_alert("**                                                         **");
+	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+	pr_alert("*************************************************************");
 #endif
 
 	ksu_core_init();
