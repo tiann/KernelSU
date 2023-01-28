3e90946e11cdce4f50f89055f97c90c70a767d3c
Ylarod <me@ylarod.cn>
2023-01-28 16:19:55 +0800

    e

---
+++ .github/scripts/build_a12.sh
@@ -0,0 +1,43 @@
+build_from_image(){
+	TITLE=kernel-aarch64-$(echo $1 | sed 's/Image-//g')
+	echo "[+] title: $TITLE"
+	echo "[+] Download prebuilt ramdisk"
+	curl -Lo gki-kernel.zip https://dl.google.com/android/gki/gki-certified-boot-android12-5.10-2021-11_r1.zip
+	unzip gki-kernel.zip && rm gki-kernel.zip
+
+	echo "[+] Unpack prebuilt boot.img"
+	BOOT_IMG=$(find . -maxdepth 1 -name "boot*.img")
+	$UNPACK_BOOTIMG --boot_img=$BOOT_IMG
+	rm $BOOT_IMG
+
+	echo "[+] Building Image.gz"
+	cat Image | $GZIP -n -f -9 > Image.gz
+
+	echo "[+] Building boot.img"
+	$MKBOOTIMG --header_version 4 --kernel Image --output boot.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level 2021-11
+	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64*1024*1024)) --image boot.img --algorithm SHA256_RSA2048 --key ../boot_sign.pem
+
+	echo "[+] Building boot-gz.img"
+	$MKBOOTIMG --header_version 4 --kernel Image.gz --output boot-gz.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level 2021-11
+	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64*1024*1024)) --image boot-gz.img --algorithm SHA256_RSA2048 --key ../boot_sign.pem
+
+	echo "[+] Building boot-lz4.img"
+	$MKBOOTIMG --header_version 4 --kernel Image.lz4 --output boot-lz4.img --ramdisk out/ramdisk --os_version 12.0.0 --os_patch_level 2021-11
+	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64*1024*1024)) --image boot-lz4.img --algorithm SHA256_RSA2048 --key ../boot_sign.pem
+
+    echo "[+] Compress images"
+    for image in boot*.img; do
+        $GZIP -n -f -9 $image
+    done
+
+	python3 $GITHUB_WORKSPACE/KernelSU/scripts/ksubot.py
+}
+
+for dir in Image*; do
+	if [ -d "$dir" ]; then
+		echo "----- Building $dir -----"
+		cd $dir
+		build_from_image $dir
+		cd ..
+	fi
+done
---
+++ .github/scripts/build_a13.sh
@@ -0,0 +1,34 @@
+build_from_image(){
+	TITLE=kernel-aarch64-$(echo $1 | sed 's/Image-//g')
+	echo "[+] title: $TITLE"
+	echo "[+] Building Image.gz"
+	cat Image | $GZIP -n -f -9 > Image.gz
+    
+	echo "[+] Building boot.img"
+	$MKBOOTIMG --header_version 4 --kernel Image --output boot.img
+	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64*1024*1024)) --image boot.img --algorithm SHA256_RSA2048 --key ../boot_sign.pem
+
+	echo "[+] Building boot-gz.img"
+	$MKBOOTIMG --header_version 4 --kernel Image.gz --output boot-gz.img
+	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64*1024*1024)) --image boot-gz.img --algorithm SHA256_RSA2048 --key ../boot_sign.pem
+
+	echo "[+] Building boot-lz4.img"
+	$MKBOOTIMG --header_version 4 --kernel Image.lz4 --output boot-lz4.img
+	$AVBTOOL add_hash_footer --partition_name boot --partition_size $((64*1024*1024)) --image boot-lz4.img --algorithm SHA256_RSA2048 --key ../boot_sign.pem
+    
+    echo "[+] Compress images"
+    for image in boot*.img; do
+        $GZIP -n -f -9 $image
+    done
+
+	find . -type f -name "*.gz" | xargs python3 $GITHUB_WORKSPACE/KernelSU/scripts/ksubot.py
+}
+
+for dir in Image*; do
+	if [ -d "$dir" ]; then
+		echo "----- Building $dir -----"
+		cd $dir
+		build_from_image $dir
+		cd ..
+	fi
+done
