efb26dedcbc42264fcafc06c2a428edc5dfc9d3e
Ookiineko <chiisaineko@protonmail.com>
2023-02-01 14:09:51 +0000

    kernel: add include guard for kernel_compat.h
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -1,3 +1,8 @@
+#ifndef __KSU_H_KERNEL_COMPAT
+#define __KSU_H_KERNEL_COMPAT
+
 #include "linux/fs.h"
+
 extern ssize_t kernel_read_compat(struct file *p, void* buf, size_t count, loff_t *pos);
 extern ssize_t kernel_write_compat(struct file *p, const void *buf, size_t count, loff_t *pos);
+#endif
