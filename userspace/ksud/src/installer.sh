3bfee10a0d4cbe8fc0a77613ed0a4ee32def1186
tiann <twsxtd@gmail.com>
2023-02-01 21:10:16 +0800

    ksud: set SELinux context for /vendor

--- userspace/ksud/src/installer.sh
+++ userspace/ksud/src/installer.sh
@@ -366,7 +366,7 @@ install_module() {
       set_perm_recursive $MODPATH/system/bin 0 2000 0755 0755
       set_perm_recursive $MODPATH/system/xbin 0 2000 0755 0755
       set_perm_recursive $MODPATH/system/system_ext/bin 0 2000 0755 0755
-      set_perm_recursive $MODPATH/system/vendor/bin 0 2000 0755 0755 u:object_r:vendor_file:s0
+      set_perm_recursive $MODPATH/system/vendor 0 2000 0755 0755 u:object_r:vendor_file:s0
     fi
 
     # Load customization script
