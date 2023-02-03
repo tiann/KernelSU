bf7827f5f8985844ca2c2b9b2d7821108914ff00
skbeh <60107333+skbeh@users.noreply.github.com>
2023-02-03 13:24:35 +0800

    ci: add CI for clippy, rustfmt and shell scripts

--- userspace/ksud/src/restorecon.rs
+++ userspace/ksud/src/restorecon.rs
@@ -9,12 +9,16 @@ use extattr::{setxattr, Flags as XattrFlags};
 
 const SYSTEM_CON: &str = "u:object_r:system_file:s0";
 const _ADB_CON: &str = "u:object_r:adb_data_file:s0";
-const SELINUX_XATTR : &str = "security.selinux";
+const SELINUX_XATTR: &str = "security.selinux";
 
 pub fn setcon<P: AsRef<Path>>(path: P, con: &str) -> Result<()> {
     #[cfg(any(target_os = "linux", target_os = "android"))]
-    setxattr(&path, SELINUX_XATTR, con, XattrFlags::empty())
-        .with_context(|| format!("Failed to change SELinux context for {}", path.as_ref().display()))?;
+    setxattr(&path, SELINUX_XATTR, con, XattrFlags::empty()).with_context(|| {
+        format!(
+            "Failed to change SELinux context for {}",
+            path.as_ref().display()
+        )
+    })?;
     Ok(())
 }
 
@@ -32,9 +36,9 @@ pub fn restore_syscon<P: AsRef<Path>>(dir: P) -> Result<()> {
     for dir_entry in WalkDir::new(dir).parallelism(Serial) {
         if let Some(path) = dir_entry.ok().map(|dir_entry| dir_entry.path()) {
             #[cfg(any(target_os = "linux", target_os = "android"))]
-            setxattr(&path, SELINUX_XATTR, SYSTEM_CON, XattrFlags::empty()).with_context(
-                || format!("Failed to change SELinux context for {}", path.display()),
-            )?;
+            setxattr(&path, SELINUX_XATTR, SYSTEM_CON, XattrFlags::empty()).with_context(|| {
+                format!("Failed to change SELinux context for {}", path.display())
+            })?;
         }
     }
     Ok(())
