cc29ad151afd69c4cc14434ef5cb2b248972e3b8
tiann <twsxtd@gmail.com>
2023-02-08 15:09:36 +0800

    ksud: Add KSU_KERNEL_VER_CODE

--- userspace/ksud/src/module.rs
+++ userspace/ksud/src/module.rs
@@ -47,6 +47,7 @@ fn exec_install_script(module_file: &str) -> Result<()> {
             format!("{}:{}", env_var("PATH").unwrap(), defs::BINARY_DIR),
         )
         .env("KSU", "true")
+        .env("KSU_KERNEL_VER_CODE", crate::ksu::get_version().to_string())
         .env("KSU_VER", defs::VERSION_NAME)
         .env("KSU_VER_CODE", defs::VERSION_CODE)
         .env("OUTFD", "1")
@@ -218,13 +219,14 @@ fn exec_script<T: AsRef<Path>>(path: T, wait: bool) -> Result<()> {
         .arg("sh")
         .arg(path.as_ref())
         .env("ASH_STANDALONE", "1")
+        .env("KSU", "true")
+        .env("KSU_KERNEL_VER_CODE", crate::ksu::get_version().to_string())
         .env("KSU_VER_CODE", defs::VERSION_CODE)
         .env("KSU_VER", defs::VERSION_NAME)
         .env(
             "PATH",
             format!("{}:{}", env_var("PATH").unwrap(), defs::BINARY_DIR),
-        )
-        .env("KSU", "true");
+        );
 
     let result = if wait {
         command.status().map(|_| ())
