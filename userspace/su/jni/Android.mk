1bb3a23bc4d575f67978da0388c10b5312d5c59c
tiann <twsxtd@gmail.com>
2023-04-04 17:24:35 +0800

    userspace: remove simple su

--- userspace/su/jni/Android.mk
+++ userspace/su/jni/Android.mk
@@ -4,8 +4,3 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := su
 LOCAL_SRC_FILES := su.c
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := simple_su
-LOCAL_SRC_FILES := simple_su.c
-include $(BUILD_EXECUTABLE)
