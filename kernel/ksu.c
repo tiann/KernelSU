5cf86c2f48c51d434f28327ac48fc374f2f2ec64
SoDebug <30922923+SoDebug@users.noreply.github.com>
2023-04-26 19:01:18 +0800

    Update ksu.c: Do not warn when kprobes are not enabled

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -57,7 +57,7 @@ int __init kernelsu_init(void)
 	ksu_enable_sucompat();
 	ksu_enable_ksud();
 #else
-#warning("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html")
+// #warning("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html")
 #endif
 
 	return 0;
