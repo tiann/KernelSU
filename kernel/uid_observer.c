95044bb551062c18fc1639af3b40afe476a6c188
weishu <twsxtd@gmail.com>
2023-05-22 10:32:20 +0800

    kernel: fix incorrect prune uid in allowlist for work profile app. close #535

--- kernel/uid_observer.c
+++ kernel/uid_observer.c
@@ -29,7 +29,7 @@ static bool is_uid_exist(uid_t uid, void *data)
 
 	bool exist = false;
 	list_for_each_entry (np, list, list) {
-		if (np->uid == uid) {
+		if (np->uid % 100000 == uid) {
 			exist = true;
 			break;
 		}
