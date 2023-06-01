08a78e39bdb4eb9e4ad4fc79936f1f37695a5462
dabao1955 <195328750@qq.com>
2023-06-01 09:11:00 +0800

    fix source kconfig again

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -45,5 +45,6 @@ echo '[+] Add kernel su driver to Makefile'
 DRIVER_MAKEFILE=$DRIVER_DIR/Makefile
 grep -q "kernelsu" "$DRIVER_MAKEFILE" || printf "\nobj-$(CONFIG_KSU) += kernelsu/\n" >> "$DRIVER_MAKEFILE"
 DRIVER_KCONFIG=$DRIVER_DIR/Kconfig
-grep -q "kernelsu" "$DRIVER_KCONFIG" || printf printf "source \"drivers/kernelsu/Kconfig\"\n" >> "$DRIVER_KCONFIG"
+KSU_CONFIG=$(printf "source \"drivers/kernelsu/Kconfig\"\n")
+grep -q "kernelsu" "$DRIVER_KCONFIG" || sed  "4i\\$text" "$DRIVER_KCONFIG"
 echo '[+] Done.'
