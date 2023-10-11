b1e08b78e38dbd1352474c066c6a32afe995fd6e
weishu <twsxtd@gmail.com>
2023-10-11 11:42:08 +0800

    ksud: tips for inconsisent signature

--- userspace/ksud/src/debug.rs
+++ userspace/ksud/src/debug.rs
@@ -1,37 +1,23 @@
 use anyhow::{ensure, Context, Ok, Result};
-use std::{
-    path::{Path, PathBuf},
-    process::Command,
-};
+use std::{path::Path, process::Command};
 
 use crate::apk_sign::get_apk_signature;
 
 const KERNEL_PARAM_PATH: &str = "/sys/module/kernelsu";
 
-fn read_u32(path: &PathBuf) -> Result<u32> {
-    let content = std::fs::read_to_string(path)?;
-    let content = content.trim();
-    let content = content.parse::<u32>()?;
-    Ok(content)
-}
-
-fn set_kernel_param(size: u32, hash: String) -> Result<()> {
+fn set_kernel_param(hash: String) -> Result<()> {
     let kernel_param_path = Path::new(KERNEL_PARAM_PATH).join("parameters");
-    let expeced_size_path = kernel_param_path.join("ksu_expected_size");
     let expeced_hash_path = kernel_param_path.join("ksu_expected_hash");
 
     println!(
-        "before size: {:#x} hash: {}",
-        read_u32(&expeced_size_path)?,
+        "before hash: {}",
         std::fs::read_to_string(&expeced_hash_path)?
     );
 
-    std::fs::write(&expeced_size_path, size.to_string())?;
     std::fs::write(&expeced_hash_path, hash)?;
 
     println!(
-        "after size: {:#x} hash: {}",
-        read_u32(&expeced_size_path)?,
+        "after hash: {}",
         std::fs::read_to_string(&expeced_hash_path)?
     );
 
@@ -56,6 +42,6 @@ pub fn set_manager(pkg: &str) -> Result<()> {
 
     let path = get_apk_path(pkg).with_context(|| format!("{pkg} does not exist!"))?;
     let sign = get_apk_signature(&path)?;
-    set_kernel_param(sign.0, sign.1)?;
+    set_kernel_param(sign)?;
     Ok(())
 }
