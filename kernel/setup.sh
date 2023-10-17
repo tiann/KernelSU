7712f770b166e8292e49bc03084c065b4e1356ff
IminatoPo <1300089753@qq.com>
2023-10-17 17:37:53 +0800

    Update setup.sh
    https://github.com/IminatoPo/KernelSU/edit/main/kernel/setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -15,7 +15,7 @@ else
      exit 127
 fi
 
-test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/tiann/KernelSU
+test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/IminatoPo/KernelSU
 cd "$GKI_ROOT/KernelSU"
 git stash
 if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
