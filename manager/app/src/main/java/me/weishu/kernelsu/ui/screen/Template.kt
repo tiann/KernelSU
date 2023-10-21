d4e19bb8fc90ef136cc20cce54b94cdfbcee9b12
weishu <twsxtd@gmail.com>
2023-10-21 17:13:23 +0800

    manager: refresh template screen if needed.

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Template.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Template.kt
@@ -1,16 +1,12 @@
 package me.weishu.kernelsu.ui.screen
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
@@ -21,7 +17,6 @@ import androidx.compose.material.icons.filled.Sync
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -35,20 +30,16 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.result.ResultRecipient
+import com.ramcosta.composedestinations.result.getOr
 import kotlinx.coroutines.launch
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.screen.destinations.TemplateEditorScreenDestination
 import me.weishu.kernelsu.ui.viewmodel.TemplateViewModel
-import java.util.Locale
 
 /**
  * @author weishu
@@ -58,13 +49,23 @@ import java.util.Locale
 @OptIn(ExperimentalMaterialApi::class)
 @Destination
 @Composable
-fun AppProfileTemplateScreen(navigator: DestinationsNavigator) {
+fun AppProfileTemplateScreen(
+    navigator: DestinationsNavigator,
+    resultRecipient: ResultRecipient<TemplateEditorScreenDestination, Boolean>
+) {
     val viewModel = viewModel<TemplateViewModel>()
     val scope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
         if (viewModel.templateList.isEmpty()) {
             viewModel.fetchTemplates()
+        }
+    }
+
+    // handle result from TemplateEditorScreen, refresh if needed
+    resultRecipient.onNavResult { result ->
+        if (result.getOr { false }) {
+            scope.launch { viewModel.fetchTemplates() }
         }
     }
 
