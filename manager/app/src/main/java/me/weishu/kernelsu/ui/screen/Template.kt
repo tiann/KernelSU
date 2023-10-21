9b294682b0ede778dcb00b73525c995d40fd9010
weishu <twsxtd@gmail.com>
2023-10-21 13:19:59 +0800

    manager: support App Profile template

---
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Template.kt
@@ -0,0 +1,148 @@
+package me.weishu.kernelsu.ui.screen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Sync
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.launch
+import me.weishu.kernelsu.R
+import me.weishu.kernelsu.ui.screen.destinations.TemplateEditorScreenDestination
+import me.weishu.kernelsu.ui.viewmodel.TemplateViewModel
+
+/**
+ * @author weishu
+ * @date 2023/10/20.
+ */
+
+@OptIn(ExperimentalMaterialApi::class)
+@Destination
+@Composable
+fun AppProfileTemplateScreen(navigator: DestinationsNavigator) {
+    val viewModel = viewModel<TemplateViewModel>()
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        if (viewModel.templateList.isEmpty()) {
+            viewModel.fetchTemplates()
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopBar(onBack = { navigator.popBackStack() },
+                onSync = {
+                    scope.launch { viewModel.fetchTemplates(true) }
+                },
+                onClickCreate = {
+                    navigator.navigate(
+                        TemplateEditorScreenDestination(
+                            TemplateViewModel.TemplateInfo(),
+                            false
+                        )
+                    )
+                })
+        },
+    ) { innerPadding ->
+        val refreshState = rememberPullRefreshState(
+            refreshing = viewModel.isRefreshing,
+            onRefresh = { scope.launch { viewModel.fetchTemplates() } },
+        )
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .pullRefresh(refreshState)
+        ) {
+            LazyColumn(Modifier.fillMaxSize()) {
+                items(viewModel.templateList, key = { it.id }) { app ->
+                    TemplateItem(navigator, app)
+                }
+            }
+
+            PullRefreshIndicator(
+                refreshing = viewModel.isRefreshing,
+                state = refreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun TemplateItem(
+    navigator: DestinationsNavigator,
+    template: TemplateViewModel.TemplateInfo
+) {
+    ListItem(
+        modifier = Modifier.clickable {
+            navigator.navigate(TemplateEditorScreenDestination(template, !template.local))
+        },
+        headlineContent = { Text(template.name) },
+        supportingContent = {
+            Column {
+                Text(template.description)
+                FlowRow {
+                    LabelText(label = "UID: ${template.uid}")
+                    LabelText(label = "GID: ${template.gid}")
+                    LabelText(label = template.context)
+                    if (template.local) {
+                        LabelText(label = "local")
+                    } else {
+                        LabelText(label = "remote")
+                    }
+                }
+            }
+        })
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar(onBack: () -> Unit, onSync: () -> Unit, onClickCreate: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(stringResource(R.string.settings_profile_template))
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = onBack
+            ) { Icon(Icons.Filled.ArrowBack, contentDescription = null) }
+        },
+        actions = {
+            IconButton(onClick = onSync) {
+                Icon(Icons.Filled.Sync, contentDescription = null)
+            }
+            IconButton(onClick = onClickCreate) {
+                Icon(Icons.Filled.Create, contentDescription = null)
+            }
+        }
+    )
+}
---
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/TemplateViewModel.kt
@@ -0,0 +1,208 @@
+package me.weishu.kernelsu.ui.viewmodel
+
+import android.os.Parcelable
+import android.util.Log
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.parcelize.Parcelize
+import me.weishu.kernelsu.Natives
+import me.weishu.kernelsu.profile.Capabilities
+import me.weishu.kernelsu.profile.Groups
+import me.weishu.kernelsu.ui.util.getAppProfileTemplate
+import me.weishu.kernelsu.ui.util.listAppProfileTemplates
+import me.weishu.kernelsu.ui.util.setAppProfileTemplate
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
+import java.text.Collator
+import java.util.Locale
+
+/**
+ * @author weishu
+ * @date 2023/10/20.
+ */
+
+//const val TEMPLATE_INDEX_URL = "https://kernelsu.org/templates/index.json"
+//const val TEMPLATE_URL = "https://kernelsu.org/templates/%s"
+
+const val TEMPLATE_INDEX_URL = "http://192.168.31.99/templates/index.json"
+const val TEMPLATE_URL = "http://192.168.31.99/templates/%s"
+const val TAG = "TemplateViewModel"
+
+class TemplateViewModel : ViewModel() {
+    companion object {
+
+        private var templates by mutableStateOf<List<TemplateInfo>>(emptyList())
+    }
+
+    @Parcelize
+    data class TemplateInfo(
+        val id: String = "",
+        val name: String = "",
+        val description: String = "",
+        val author: String = "",
+        val local: Boolean = true,
+
+        val namespace: Int = Natives.Profile.Namespace.INHERITED.ordinal,
+        val uid: Int = 0,
+        val gid: Int = 0,
+        val groups: List<Int> = mutableListOf(),
+        val capabilities: List<Int> = mutableListOf(),
+        val context: String = "u:r:su:s0",
+        val rules: List<String> = mutableListOf(),
+    ) : Parcelable
+
+    var isRefreshing by mutableStateOf(false)
+        private set
+
+    val templateList by derivedStateOf {
+        val comparator = compareBy(TemplateInfo::local).then(
+            compareBy(
+                Collator.getInstance(Locale.getDefault()), TemplateInfo::name
+            )
+        )
+        templates.sortedWith(comparator).apply {
+            isRefreshing = false
+        }
+    }
+
+    suspend fun fetchTemplates(sync: Boolean = false) {
+        isRefreshing = true
+        withContext(Dispatchers.IO) {
+            val localTemplateIds = listAppProfileTemplates()
+            Log.i(TAG, "localTemplateIds: $localTemplateIds")
+            if (localTemplateIds.isEmpty() || sync) {
+                // if no templates, fetch remote templates
+                fetchRemoteTemplates()
+            }
+
+            // fetch templates again
+            templates = listAppProfileTemplates().mapNotNull(::getTemplateInfoById)
+
+            isRefreshing = false
+        }
+    }
+}
+
+private fun fetchRemoteTemplates() {
+    OkHttpClient().newCall(
+        Request.Builder().url(TEMPLATE_INDEX_URL).build()
+    ).runCatching {
+        execute().use { response ->
+            if (!response.isSuccessful) {
+                return
+            }
+            val remoteTemplateIds = JSONArray(response.body!!.string())
+            Log.i(TAG, "fetchRemoteTemplates: $remoteTemplateIds")
+            0.until(remoteTemplateIds.length()).forEach { i ->
+                val id = remoteTemplateIds.getString(i)
+                val templateJson = OkHttpClient().newCall(
+                    Request.Builder().url(TEMPLATE_URL.format(id)).build()
+                ).runCatching {
+                    execute().use { response ->
+                        if (!response.isSuccessful) {
+                            return@forEach
+                        }
+                        response.body!!.string()
+                    }
+                }.getOrNull() ?: return@forEach
+                Log.i(TAG, "template: $templateJson")
+
+                // validate remote template
+                runCatching {
+                    val json = JSONObject(templateJson)
+                    fromJSON(json)?.let {
+                        // force local template
+                        json.put("local", false)
+                        setAppProfileTemplate(id, json.toString())
+                    }
+                }.onFailure {
+                    Log.e(TAG, "ignore invalid template: $it", it)
+                    return@forEach
+                }
+            }
+        }
+    }.onFailure {
+        Log.e(TAG, "fetchRemoteTemplates error", it)
+    }
+}
+
+private fun <T, R> JSONArray.mapCatching(
+    transform: (T) -> R, onFail: (Throwable) -> Unit
+): List<R> {
+    return List(length()) { i -> get(i) as T }.mapNotNull { element ->
+        runCatching {
+            transform(element)
+        }.onFailure(onFail).getOrNull()
+    }
+}
+
+private inline fun <reified T : Enum<T>> getEnumOrdinals(
+    jsonArray: JSONArray, enumClass: Class<T>
+): List<T> {
+    return jsonArray.mapCatching<String, T>({ name ->
+        enumValueOf(name.uppercase())
+    }, {
+        Log.e(TAG, "ignore invalid enum ${enumClass.simpleName}: $it", it)
+    })
+}
+
+fun getTemplateInfoById(id: String): TemplateViewModel.TemplateInfo? {
+    return runCatching {
+        fromJSON(JSONObject(getAppProfileTemplate(id)))
+    }.onFailure {
+        Log.e(TAG, "ignore invalid template: $it", it)
+    }.getOrNull()
+}
+
+private fun fromJSON(templateJson: JSONObject): TemplateViewModel.TemplateInfo? {
+    return runCatching {
+        val groupsJsonArray = templateJson.getJSONArray("groups")
+        val capabilitiesJsonArray = templateJson.getJSONArray("capabilities")
+        val rulesJsonArray = templateJson.optJSONArray("rules")
+        val templateInfo = TemplateViewModel.TemplateInfo(
+            id = templateJson.getString("id"),
+            name = templateJson.getString("name"),
+            description = templateJson.getString("description"),
+            local = templateJson.getBoolean("local"),
+            namespace = Natives.Profile.Namespace.valueOf(
+                templateJson.getString("namespace").uppercase()
+            ).ordinal,
+            uid = templateJson.getInt("uid"),
+            gid = templateJson.getInt("gid"),
+            groups = getEnumOrdinals(groupsJsonArray, Groups::class.java).map { it.gid },
+            capabilities = getEnumOrdinals(
+                capabilitiesJsonArray, Capabilities::class.java
+            ).map { it.cap },
+            context = templateJson.getString("context"),
+            rules = rulesJsonArray?.mapCatching<String, String>({ it }, {
+                Log.e(TAG, "ignore invalid rule: $it", it)
+            }).orEmpty()
+        )
+        templateInfo
+    }.onFailure {
+        Log.e(TAG, "ignore invalid template: $it", it)
+    }.getOrNull()
+}
+
+fun generateTemplates() {
+    val templateJson = JSONObject()
+    templateJson.put("id", "com.example")
+    templateJson.put("name", "Example")
+    templateJson.put("description", "This is an example template")
+    templateJson.put("local", true)
+    templateJson.put("namespace", Natives.Profile.Namespace.INHERITED.name)
+    templateJson.put("uid", 0)
+    templateJson.put("gid", 0)
+
+    templateJson.put("groups", JSONArray().apply { put(Groups.INET.name) })
+    templateJson.put("capabilities", JSONArray().apply { put(Capabilities.CAP_NET_RAW.name) })
+    templateJson.put("context", "u:r:su:s0")
+    Log.i(TAG, "$templateJson")
+}
