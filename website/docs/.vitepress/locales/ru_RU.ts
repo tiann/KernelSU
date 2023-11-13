c74880d47f9ed29c6cc80a5f8210bd2dbf07644e
igor <134963561+igormiguell@users.noreply.github.com>
2023-11-13 17:46:32 -0300

    Fix GitHub name

--- website/docs/.vitepress/locales/ru_RU.ts
+++ website/docs/.vitepress/locales/ru_RU.ts
@@ -36,7 +36,7 @@ export default defineConfig({
 function nav() {
   return [
     { text: 'Руководство', link: '/ru_RU/guide/what-is-kernelsu' },
-    { text: 'Github', link: 'https://github.com/tiann/KernelSU' }
+    { text: 'GitHub', link: 'https://github.com/tiann/KernelSU' }
   ]
 }
 
