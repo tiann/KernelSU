fe65c2a3687289af74a3070061cc45bc3ace1e3b
weishu <twsxtd@gmail.com>
2023-11-14 11:16:40 +0800

    website: remove github nav becuase it is already in social links

--- website/docs/.vitepress/locales/pt_BR.ts
+++ website/docs/.vitepress/locales/pt_BR.ts
@@ -36,7 +36,6 @@ export default defineConfig({
 function nav() {
   return [
     { text: 'Guia', link: '/pt_BR/guide/what-is-kernelsu' },
-    { text: 'GitHub', link: 'https://github.com/tiann/KernelSU' }
   ]
 }
 
