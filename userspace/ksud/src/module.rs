b612efcfadf764aaddc5b9634b4dcb6b08b3f664
weishu <twsxtd@gmail.com>
2024-01-29 21:49:39 +0800

    ksud: sparse file default 256G

--- userspace/ksud/src/module.rs
+++ userspace/ksud/src/module.rs
@@ -339,7 +339,7 @@ fn _install_module(zip: &str) -> Result<()> {
         humansize::format_size(zip_uncompressed_size, humansize::DECIMAL)
     );
 
-    let sparse_image_size = 1024 * (1 << 30); // 1T
+    let sparse_image_size = 256 * (1 << 30); // 256G
     if !modules_img_exist && !modules_update_img_exist {
         // if no modules and modules_update, it is brand new installation, we should create a new img
         // create a tmp module img and mount it to modules_update
