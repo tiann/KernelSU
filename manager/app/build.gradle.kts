ecaed359e45035b69d6f72c6358f6ded5f263883
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2024-03-11 15:08:29 +0300

    Update

--- manager/app/build.gradle.kts
+++ manager/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.10"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
 
     packaging {
