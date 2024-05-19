1760cd0e84e922bc8aace959da6e964e48cad28b
Alex <alex5402private@gmail.com>
2024-05-19 10:42:30 +0530

    Update build.gradle.kts

--- manager/app/build.gradle.kts
+++ manager/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
