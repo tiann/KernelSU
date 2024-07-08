0106628da1f54d41d3fc0afab0edb55bdea149d4
LoveSy <shana@zju.edu.cn>
2024-07-08 22:49:18 +0800

    Upgrade gradle (#1866)

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -15,29 +15,23 @@ cmaker {
     default {
         arguments.addAll(
             arrayOf(
-                "-DANDROID_STL=c++_static",
+                "-DANDROID_STL=none",
             )
         )
-        val flags = arrayOf(
-            "-Wno-gnu-string-literal-operator-template",
-            "-Wno-c++2b-extensions",
-        )
-        cFlags.addAll(flags)
-        cppFlags.addAll(flags)
-        abiFilters("arm64-v8a", "x86_64")
+        abiFilters("arm64-v8a", "x86_64", "riscv64")
     }
     buildTypes {
         if (it.name == "release") {
-            arguments += "-DDEBUG_SYMBOLS_PATH=${buildDir.absolutePath}/symbols"
+            arguments += "-DDEBUG_SYMBOLS_PATH=${layout.buildDirectory.asFile.get().absolutePath}/symbols"
         }
     }
 }
 
 val androidMinSdkVersion = 26
-val androidTargetSdkVersion = 34
-val androidCompileSdkVersion = 34
-val androidBuildToolsVersion = "34.0.0"
-val androidCompileNdkVersion = "26.3.11579264"
+val androidTargetSdkVersion = 35
+val androidCompileSdkVersion = 35
+val androidBuildToolsVersion = "35.0.0"
+val androidCompileNdkVersion = "27.0.11718014-beta1"
 val androidSourceCompatibility = JavaVersion.VERSION_21
 val androidTargetCompatibility = JavaVersion.VERSION_21
 val managerVersionCode by extra(getVersionCode())
