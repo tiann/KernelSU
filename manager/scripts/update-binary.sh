f98937630873cccfd2f930f842b0c851412cc2c5
SakuraKyuo <sakurakyuo@protonmail.com>
2024-08-02 06:47:39 +0800

    manager: Add flash script into KernelSU Manager
    Add flash script and libbusybox.so into KernelSU Manager, with that, users can more easier to get KernelSU like how they get Magisk.
    
    Test: Build Manager - Flash in Third-Party Recovery - Reboot - Check KernelSU
    
    Result: It works well on GKI Devices(Tested on Oneplus Ace 3 Pro)
    
    Needs to improve:
    - Not sure some Xiaomi Devices will works well or not, waiting for test.
    - Need to add Non-GKI devices detecte
    
    Signed-off-by: SakuraKyuo <SakuraKyuo@protonmail.com>

---
+++ manager/scripts/update-binary.sh
@@ -0,0 +1,25 @@
+#!/sbin/sh
+
+TMPDIR=/dev/tmp
+rm -rf $TMPDIR
+mkdir -p $TMPDIR 2>/dev/null
+
+export BBBIN=$TMPDIR/busybox
+unzip -o "$3" "busybox" -d $TMPDIR >&2
+
+for arch in  "arm64-v8a" ; do
+  unzip -o "$3" "lib/$arch/libbusybox.so" -d $TMPDIR >&2
+  libpath="$TMPDIR/lib/$arch/libbusybox.so"
+  chmod 755 $libpath
+  if [ -x $libpath ] && $libpath >/dev/null 2>&1; then
+    mv -f $libpath $BBBIN
+    break
+  fi
+done
+$BBBIN rm -rf $TMPDIR/lib
+
+export INSTALLER=$TMPDIR/install
+$BBBIN mkdir -p $INSTALLER
+$BBBIN unzip -o "$3" "addon/*" "META-INF/com/google/*" "lib/*" "META-INF/com/google/*" -x "lib/*/libbusybox.so" -d $INSTALLER >&2
+export ASH_STANDALONE=1
+exec $BBBIN sh "$INSTALLER/META-INF/com/google/android/updater-script" "$@"
