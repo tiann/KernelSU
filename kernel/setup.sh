91b69c9123b66b3e5d4408cf1d80d87ea479fbd0
Rifat Azad <rifat.44.azad.rifs@gmail.com>
2024-12-04 16:47:58 +0600

    kernel: setup next branch

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -43,7 +43,7 @@ setup_kernelsu() {
     cd "$GKI_ROOT/KernelSU"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout main && echo "[-] Switched to main branch."
+        git checkout next && echo "[-] Switched to next branch."
     fi
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
