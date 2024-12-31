0a617959f9c95cc8a70d104c8acada9de415a83f
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2024-12-31 16:30:58 +0800

    manager: support search module list (#2331)

--- manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleViewModel.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleViewModel.kt
@@ -10,10 +10,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import me.weishu.kernelsu.ui.util.HanziToPinyin
 import me.weishu.kernelsu.ui.util.listModules
 import me.weishu.kernelsu.ui.util.overlayFsAvailable
 import org.json.JSONArray
 import org.json.JSONObject
+import java.text.Collator
+import java.util.Locale
 
 class ModuleViewModel : ViewModel() {
 
@@ -46,6 +49,7 @@ class ModuleViewModel : ViewModel() {
 
     var isRefreshing by mutableStateOf(false)
         private set
+    var search by mutableStateOf("")
 
     var isOverlayAvailable by mutableStateOf(overlayFsAvailable())
         private set
@@ -57,8 +61,11 @@ class ModuleViewModel : ViewModel() {
             compareBy<ModuleInfo>(
                 { if (sortEnabledFirst) !it.enabled else 0 },
                 { if (sortActionFirst) !it.hasWebUi && !it.hasActionScript else 0 },
-                { it.id })
-        modules.sortedWith(comparator).also {
+            ).thenBy(Collator.getInstance(Locale.getDefault()), ModuleInfo::id)
+        modules.filter {
+            it.id.contains(search, true) || it.name.contains(search, true) || HanziToPinyin.getInstance()
+                .toPinyinString(it.name).contains(search, true)
+        }.sortedWith(comparator).also {
             isRefreshing = false
         }
     }
