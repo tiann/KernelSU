a953ae97b72197205271721413394f883aa26a8d
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2025-01-24 18:46:39 +0800

    ksud: fix copy sparse file (#2374)

--- userspace/ksud/src/utils.rs
+++ userspace/ksud/src/utils.rs
@@ -252,7 +252,7 @@ pub fn copy_sparse_file<P: AsRef<Path>, Q: AsRef<Path>>(
     for segment in segments {
         if let SegmentType::Data = segment.segment_type {
             let start = segment.start;
-            let end = segment.end;
+            let end = segment.end + 1;
 
             src_file.seek(SeekFrom::Start(start))?;
             dst_file.seek(SeekFrom::Start(start))?;
