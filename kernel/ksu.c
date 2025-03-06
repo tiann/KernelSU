c90d31881f4be2d3157b1c7f3a941816e65e7e50
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-06 12:32:57 +0800

    Revert "kernel: remove unused CONFIG guard becuase GKI kernel enable kprobe by default"
    This reverts commit 500ff9bcbca59ed6d292aaf24f57713c808bef74.

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -57,8 +57,12 @@ int __init kernelsu_init(void)
 
 	ksu_throne_tracker_init();
 
+#ifdef CONFIG_KPROBES
 	ksu_sucompat_init();
 	ksu_ksud_init();
+#else
+	pr_alert("KPROBES is disabled, KernelSU may not work, please check https://kernelsu.org/guide/how-to-integrate-for-non-gki.html");
+#endif
 
 #ifdef MODULE
 #ifndef CONFIG_KSU_DEBUG
@@ -76,8 +80,10 @@ void kernelsu_exit(void)
 
 	destroy_workqueue(ksu_workqueue);
 
+#ifdef CONFIG_KPROBES
 	ksu_ksud_exit();
 	ksu_sucompat_exit();
+#endif
 
 	ksu_core_exit();
 }
