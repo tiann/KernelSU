97b2411316c749d3b5385e8c054d206aa741ae31
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-12 08:33:09 +0800

    Revert "kernel: remove unused CONFIG guard becuase GKI kernel enable kprobe by default" (#2495)
    follow up to
    https://github.com/tiann/KernelSU/pull/2475#issuecomment-2680947145

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -886,7 +886,9 @@ void __init ksu_core_init(void)
 
 void ksu_core_exit(void)
 {
+#ifdef CONFIG_KPROBES
 	pr_info("ksu_core_kprobe_exit\n");
 	// we dont use this now
 	// ksu_kprobe_exit();
+#endif
 }
--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -189,6 +189,8 @@ int ksu_handle_devpts(struct inode *inode)
 	return 0;
 }
 
+#ifdef CONFIG_KPROBES
+
 static int faccessat_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
 	struct pt_regs *real_regs = PT_REAL_REGS(regs);
@@ -261,19 +263,24 @@ static void destroy_kprobe(struct kprobe **kp_ptr)
 }
 
 static struct kprobe *su_kps[4];
+#endif
 
 // sucompat: permited process can execute 'su' to gain root access.
 void ksu_sucompat_init()
 {
+#ifdef CONFIG_KPROBES
 	su_kps[0] = init_kprobe(SYS_EXECVE_SYMBOL, execve_handler_pre);
 	su_kps[1] = init_kprobe(SYS_FACCESSAT_SYMBOL, faccessat_handler_pre);
 	su_kps[2] = init_kprobe(SYS_NEWFSTATAT_SYMBOL, newfstatat_handler_pre);
 	su_kps[3] = init_kprobe("pts_unix98_lookup", pts_unix98_lookup_pre);
+#endif
 }
 
 void ksu_sucompat_exit()
 {
+#ifdef CONFIG_KPROBES
 	for (int i = 0; i < ARRAY_SIZE(su_kps); i++) {
 		destroy_kprobe(&su_kps[i]);
 	}
+#endif
 }
