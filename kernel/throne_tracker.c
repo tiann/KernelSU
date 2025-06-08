f08e15910363f19bdd364249f47040b3ff5e28c9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-09 07:23:35 +0800

    kernel: throne_tracker: avoid cross-fs traversal using s_magic check
    Skip directories that does NOT have the same magic as /data/app.
    This is to avoid scanning incfs and any other stacked filesystems.
    
    While this is way dumber, it's way cheaper.
    no kern_path(), no missable path_put(), no ref handling.
    
    This supercedes
    `throne_tracker: avoid cross fs access (https://github.com/tiann/KernelSU/pull/2626)`
    - upstream https://github.com/tiann/KernelSU/commit/0b6998b474ed00610bbf2d6679f853cef07af3b8
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/throne_tracker.c
+++ kernel/throne_tracker.c
@@ -213,7 +213,8 @@ void search_manager(const char *path, int depth, struct list_head *uid_data)
 	int i, stop = 0;
 	struct list_head data_path_list;
 	INIT_LIST_HEAD(&data_path_list);
-
+	static unsigned long data_app_magic __read_mostly = 0;
+	
 	// Initialize APK cache list
 	struct apk_path_hash *pos, *n;
 	list_for_each_entry(pos, &apk_path_hash_list, list) {
@@ -242,6 +243,22 @@ void search_manager(const char *path, int depth, struct list_head *uid_data)
 				file = ksu_filp_open_compat(pos->dirpath, O_RDONLY | O_NOFOLLOW, 0);
 				if (IS_ERR(file)) {
 					pr_err("Failed to open directory: %s, err: %ld\n", pos->dirpath, PTR_ERR(file));
+					goto skip_iterate;
+				}
+				
+				// grab magic on first folder, which is /data/app
+				if (unlikely(!data_app_magic)) {
+					if (file->f_inode->i_sb->s_magic) {
+						data_app_magic = file->f_inode->i_sb->s_magic;
+						pr_info("%s: dir: %s got magic! 0x%lx\n", __func__, pos->dirpath, data_app_magic);
+					} else
+						goto skip_iterate;
+				}
+				
+				if (file->f_inode->i_sb->s_magic != data_app_magic) {
+					pr_info("%s: skip: %s magic: 0x%lx expected: 0x%lx\n", __func__, pos->dirpath, 
+						file->f_inode->i_sb->s_magic, data_app_magic);
+					filp_close(file, NULL);
 					goto skip_iterate;
 				}
 
