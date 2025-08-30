e98823017eb9032f36d24e1ae2758de054d371da
YumeYuka <Miao@YumeYuka.plus>
2025-08-30 21:51:18 +0800

    manager: optimize webui
    (cherry picked from commit 52eec80e1f2dc98ce4bb49e24f132191a0133c33)

--- manager/app/src/main/java/me/weishu/kernelsu/ui/webui/WebUIActivity.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/webui/WebUIActivity.kt
@@ -33,8 +33,8 @@ class WebUIActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        val moduleId = intent.getStringExtra("id") ?: run { finishAndRemoveTask(); return }
-        val name = intent.getStringExtra("name") ?: run { finishAndRemoveTask(); return }
+        val moduleId = intent.getStringExtra("id") ?: finishAndRemoveTask().let { return }
+        val name = intent.getStringExtra("name") ?: finishAndRemoveTask().let { return }
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
             @Suppress("DEPRECATION")
             setTaskDescription(ActivityManager.TaskDescription("KernelSU - $name"))
@@ -91,10 +91,11 @@ class WebUIActivity : ComponentActivity() {
 
     override fun onDestroy() {
         rootShell.runCatching { close() }
-        webView = webView?.apply {
+        webView?.apply {
             stopLoading()
             removeAllViews()
             destroy()
+            webView = null
         }
         super.onDestroy()
     }
