20b33b8dd3b35a31d0ad6cc7eda1b0c6d34c3b7d
weishu <twsxtd@gmail.com>
2025-10-31 14:57:29 +0800

    ksud: fix cargo clippy

--- userspace/ksud/src/boot_patch.rs
+++ userspace/ksud/src/boot_patch.rs
@@ -118,12 +118,11 @@ fn parse_kmi_from_kernel(kernel: &PathBuf, workdir: &Path) -> Result<String> {
     let re =
         Regex::new(r"(?:.* )?(\d+\.\d+)(?:\S+)?(android\d+)").context("Failed to compile regex")?;
     for s in printable_strings {
-        if let Some(caps) = re.captures(s) {
-            if let (Some(kernel_version), Some(android_version)) = (caps.get(1), caps.get(2)) {
+        if let Some(caps) = re.captures(s)
+            && let (Some(kernel_version), Some(android_version)) = (caps.get(1), caps.get(2)) {
                 let kmi = format!("{}-{}", android_version.as_str(), kernel_version.as_str());
                 return Ok(kmi);
             }
-        }
     }
     println!("- Failed to get KMI version");
     bail!("Try to choose LKM manually")
@@ -502,11 +501,10 @@ fn do_patch(
     )?;
 
     #[cfg(target_os = "android")]
-    if need_backup {
-        if let Err(e) = do_backup(&magiskboot, workdir, ramdisk, bootimage) {
+    if need_backup
+        && let Err(e) = do_backup(&magiskboot, workdir, ramdisk, bootimage) {
             println!("- Backup stock image failed: {e}");
         }
-    }
 
     println!("- Repacking boot image");
     // magiskboot repack boot.img
