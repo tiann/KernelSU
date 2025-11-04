5284bb13e18d30b8580c44c7dbf4627272c551d2
weishu <twsxtd@gmail.com>
2025-11-04 14:14:58 +0000

    kernel: init/exit umount feature

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -488,6 +488,9 @@ void __init ksu_core_init(void)
         pr_err("ksu_kprobe_init failed: %d\n", rc);
     }
 #endif
+    if (ksu_register_feature_handler(&kernel_umount_handler)) {
+        pr_err("Failed to register umount feature handler\n");
+    }
 }
 
 void ksu_core_exit(void)
@@ -496,4 +499,5 @@ void ksu_core_exit(void)
 #ifdef CONFIG_KPROBES
     ksu_kprobe_exit();
 #endif
+    ksu_unregister_feature_handler(KSU_FEATURE_KERNEL_UMOUNT);
 }
