fa3d858996892780808cc8a740b08c2e9f46b2c6
libingxuan <84086386+aaaaaaaa-815@users.noreply.github.com>
2025-11-05 22:35:18 +0800

    support mainline kernel (#2869)

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -89,4 +89,9 @@ module_exit(kernelsu_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
 MODULE_DESCRIPTION("Android KernelSU");
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#else
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
+
