e1b4f06ff16e2c90db9839e27e8f1b8ba42d3e12
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2025-11-06 23:29:58 +0800

    kernel & ksud: add devpts fd wrapper
    This feature is intended to resolve devpts problem.

--- kernel/selinux/selinux.h
+++ kernel/selinux/selinux.h
@@ -19,7 +19,7 @@ bool is_zygote(const struct cred* cred);
 
 void apply_kernelsu_rules();
 
-u32 ksu_get_devpts_sid();
+u32 ksu_get_ksu_file_sid();
 
 int handle_sepolicy(unsigned long arg3, void __user *arg4);
 
--- kernel/supercalls.c
+++ kernel/supercalls.c
@@ -18,6 +18,8 @@
 #include "manager.h"
 #include "selinux/selinux.h"
 #include "core_hook.h"
+#include "objsec.h"
+#include "file_wrapper.h"
 
 // Permission check functions
 bool only_manager(void)
@@ -322,6 +324,66 @@ static int do_set_feature(void __user *arg)
     return 0;
 }
 
+static int do_get_wrapper_fd(void __user *arg) {
+    if (!ksu_file_sid) {
+        return -1;
+    }
+
+    struct ksu_get_wrapper_fd_cmd cmd;
+    int ret;
+
+    if (copy_from_user(&cmd, arg, sizeof(cmd))) {
+        pr_err("get_wrapper_fd: copy_from_user failed\n");
+        return -EFAULT;
+    }
+
+    struct file* f = fget(cmd.fd);
+    if (!f) {
+        return -EBADF;
+    }
+
+    struct ksu_file_wrapper *data = ksu_create_file_wrapper(f);
+    if (data == NULL) {
+        ret = -ENOMEM;
+        goto put_orig_file;
+    }
+
+    struct file* pf = anon_inode_getfile("[ksu_fdwrapper]", &data->ops, data, f->f_flags);
+    if (IS_ERR(pf)) {
+        ret = PTR_ERR(pf);
+        pr_err("ksu_fdwrapper: anon_inode_getfile failed: %ld\n", PTR_ERR(pf));
+        goto put_wrapper_data;
+    }
+
+    struct inode* wrapper_inode = file_inode(pf);
+    struct inode_security_struct *sec = selinux_inode(wrapper_inode);
+    if (sec) {
+        sec->sid = ksu_file_sid;
+    }
+
+    ret = get_unused_fd_flags(cmd.flags);
+    if (ret < 0) {
+        pr_err("ksu_fdwrapper: get unused fd failed: %d\n", ret);
+        goto put_wrapper_file;
+    }
+
+    fd_install(ret, pf);
+
+    pr_info("ksu_fdwrapper: installed wrapper fd for %p %d (flags=%d, mode=%d) to %p %d (flags=%d, mode=%d)",
+        f, cmd.fd, f->f_flags, f->f_mode, pf, ret, pf->f_flags, pf->f_mode);
+
+    goto put_orig_file;
+
+put_wrapper_file:
+    fput(pf);
+put_wrapper_data:
+    ksu_delete_file_wrapper(data);
+put_orig_file:
+    fput(f);
+
+    return ret;
+}
+
 // IOCTL handlers mapping table
 static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
     { .cmd = KSU_IOCTL_GRANT_ROOT, .name = "GRANT_ROOT", .handler = do_grant_root, .perm_check = allowed_for_su },
@@ -338,6 +400,7 @@ static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
     { .cmd = KSU_IOCTL_SET_APP_PROFILE, .name = "SET_APP_PROFILE", .handler = do_set_app_profile, .perm_check = only_manager },
     { .cmd = KSU_IOCTL_GET_FEATURE, .name = "GET_FEATURE", .handler = do_get_feature, .perm_check = manager_or_root },
     { .cmd = KSU_IOCTL_SET_FEATURE, .name = "SET_FEATURE", .handler = do_set_feature, .perm_check = manager_or_root },
+    { .cmd = KSU_IOCTL_GET_WRAPPER_FD, .name = "GET_WRAPPER_FD", .handler = do_get_wrapper_fd, .perm_check = manager_or_root },
     { .cmd = 0, .name = NULL, .handler = NULL, .perm_check = NULL } // Sentinel
 };
 
