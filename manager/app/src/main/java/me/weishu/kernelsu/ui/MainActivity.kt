13390ba12952ffdf8635949684dfeb6b00ca1e09
AlexLiuDev233 <wzylin11@outlook.com>
2025-11-09 19:34:35 +0800

    manager: if manager incompatible with current kernel, don't save ksud (#2895)
    I think we should'nt install kernelsu's userspace when manager
    incompatible with current kernel
    this maybe cause a lot of bug, for example, when user install 2.x
    kernelsu manager, but not update
    his kernel to 2.x, ksud will return "kernel version 0", because ksud
    incompatible with old supercall impl

--- manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val isManager = Natives.isManager
-        if (isManager) install()
+        if (isManager && !Natives.requireNewKernel()) install()
 
         setContent {
             KernelSUTheme {
