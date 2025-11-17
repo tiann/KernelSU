463d6a5f9886fafe7d8d41f89849c0c498852a48
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-18 00:29:18 +0800

    Update supercalls.c

--- kernel/supercalls.c
+++ kernel/supercalls.c
@@ -482,101 +482,96 @@ struct list_head mount_list = LIST_HEAD_INIT(mount_list);
 
 static int add_try_umount(void __user *arg)
 {
-	struct mount_entry *new_entry, *entry, *tmp;
-	struct ksu_add_try_umount_cmd cmd;
-	char buf[256] = {0};
+    struct mount_entry *new_entry, *entry, *tmp;
+    struct ksu_add_try_umount_cmd cmd;
+    char buf[256] = {0};
 
-	if (copy_from_user(&cmd, arg, sizeof cmd))
-		return -EFAULT;
+    if (copy_from_user(&cmd, arg, sizeof cmd))
+        return -EFAULT;
 
-	switch (cmd.mode) {
-		case KSU_UMOUNT_WIPE: {
-			struct mount_entry *entry, *tmp;
-			list_for_each_entry_safe(entry, tmp, &mount_list, list) {
-				pr_info("wipe_umount_list: removing entry: %s\n", entry->umountable);
-				list_del(&entry->list);
-				kfree(entry->umountable);
-				kfree(entry);
-			}
+    switch (cmd.mode) {
+        case KSU_UMOUNT_WIPE: {
+            struct mount_entry *entry, *tmp;
+            list_for_each_entry_safe(entry, tmp, &mount_list, list) {
+                pr_info("wipe_umount_list: removing entry: %s\n", entry->umountable);
+                list_del(&entry->list);
+                kfree(entry->umountable);
+                kfree(entry);
+            }
 
-			return 0;
-		}
+            return 0;
+        }
 
-		case KSU_UMOUNT_ADD: {
-			long len = strncpy_from_user(buf, (const char __user *)cmd.arg, 256);
-			if (len <= 0)
-				return -EFAULT;	
-			
-			buf[sizeof(buf) - 1] = '\0';
+        case KSU_UMOUNT_ADD: {
+            long len = strncpy_from_user(buf, (const char __user *)cmd.arg, 256);
+            if (len <= 0)
+                return -EFAULT;    
+            
+            buf[sizeof(buf) - 1] = '\0';
 
-			new_entry = kmalloc(sizeof(*new_entry), GFP_KERNEL);
-			if (!new_entry)
-				return -ENOMEM;
+            new_entry = kmalloc(sizeof(*new_entry), GFP_KERNEL);
+            if (!new_entry)
+                return -ENOMEM;
 
-			new_entry->umountable = kstrdup(buf, GFP_KERNEL);
-			if (!new_entry->umountable) {
-				kfree(new_entry);
-				return -1;
-			}
+            new_entry->umountable = kstrdup(buf, GFP_KERNEL);
+            if (!new_entry->umountable) {
+                kfree(new_entry);
+                return -1;
+            }
 
-			// disallow dupes
-			// if this gets too many, we can consider moving this whole task to a kthread
-			list_for_each_entry(entry, &mount_list, list) {
-				if (!strcmp(entry->umountable, buf)) {
-					pr_info("cmd_add_try_umount: %s is already here!\n", buf);
-					kfree(new_entry->umountable);
-					kfree(new_entry);
-					return -1;
-				}
-			}
-/*
-#define MNT_FORCE	0x00000001
-#define MNT_DETACH	0x00000002
-#define MNT_EXPIRE	0x00000004
-#define UMOUNT_NOFOLLOW	0x00000008
-#define UMOUNT_UNUSED	0x80000000
-*/
-			// now check flags and add
-			if (cmd.flags)
-				new_entry->flags = cmd.flags;
-			else
-				new_entry->flags = 0;
+            // disallow dupes
+            // if this gets too many, we can consider moving this whole task to a kthread
+            list_for_each_entry(entry, &mount_list, list) {
+                if (!strcmp(entry->umountable, buf)) {
+                    pr_info("cmd_add_try_umount: %s is already here!\n", buf);
+                    kfree(new_entry->umountable);
+                    kfree(new_entry);
+                    return -1;
+                }
+            }
 
-			// debug
-			list_add(&new_entry->list, &mount_list);
-			pr_info("cmd_add_try_umount: %s added!\n", buf);
+            // now check flags and add
+            // this also serves as a null check
+            if (cmd.flags)
+                new_entry->flags = cmd.flags;
+            else
+                new_entry->flags = 0;
 
-			return 0;
-		}
+            // debug
+            list_add(&new_entry->list, &mount_list);
+            pr_info("cmd_add_try_umount: %s added!\n", buf);
 
-		case KSU_UMOUNT_DEL: {
-			long len = strncpy_from_user(buf, (const char __user *)cmd.arg, sizeof(buf) - 1);
-			if (len <= 0)
-				return -EFAULT;
-			
-			buf[sizeof(buf) - 1] = '\0';
+            return 0;
+        }
 
-			list_for_each_entry_safe(entry, tmp, &mount_list, list) {
-				if (!strcmp(entry->umountable, buf)) {
-					pr_info("cmd_add_try_umount: entry removed: %s\n", entry->umountable);
-					list_del(&entry->list);
-					kfree(entry->umountable);
-					kfree(entry);
-				}
-			}
-			
-			return 0;
-		}
-		
-		default: {
-			pr_err("cmd_add_try_umount: invalid operation %u\n", cmd.mode);
-			return -EINVAL;
-		}
+        // this is just strcmp'd wipe anyway
+        case KSU_UMOUNT_DEL: {
+            long len = strncpy_from_user(buf, (const char __user *)cmd.arg, sizeof(buf) - 1);
+            if (len <= 0)
+                return -EFAULT;
+            
+            buf[sizeof(buf) - 1] = '\0';
 
-		
-	}
-	
-	return 0;
+            list_for_each_entry_safe(entry, tmp, &mount_list, list) {
+                if (!strcmp(entry->umountable, buf)) {
+                    pr_info("cmd_add_try_umount: entry removed: %s\n", entry->umountable);
+                    list_del(&entry->list);
+                    kfree(entry->umountable);
+                    kfree(entry);
+                }
+            }
+            
+            return 0;
+        }
+        
+        default: {
+            pr_err("cmd_add_try_umount: invalid operation %u\n", cmd.mode);
+            return -EINVAL;
+        }
+
+    } // switch(cmd.mode)
+    
+    return 0;
 }
 
 // IOCTL handlers mapping table
