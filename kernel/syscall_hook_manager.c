f45811e60dcb84af50017df44737a5093b5ffebe
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-12-03 19:11:43 +0800

    fix log newline & indent

--- kernel/syscall_hook_manager.c
+++ kernel/syscall_hook_manager.c
@@ -262,13 +262,13 @@ int ksu_handle_init_mark_tracker(const char __user **filename_user)
         pr_info("ksu_handle_init_mark_tracker: %ld\n", ret);
     }
 
-	if (unlikely(strcmp(path, KSUD_PATH) == 0)) {
-		pr_info("hook_manager: escape to root for init executing ksud: %d", current->pid);
-		escape_to_root_for_init();
-	} else if (likely(strstr(path, "/app_process") == NULL && strstr(path, "/adbd") == NULL)) {
-		pr_info("hook_manager: unmark %d exec %s", current->pid, path);
+    if (unlikely(strcmp(path, KSUD_PATH) == 0)) {
+        pr_info("hook_manager: escape to root for init executing ksud: %d\n", current->pid);
+        escape_to_root_for_init();
+    } else if (likely(strstr(path, "/app_process") == NULL && strstr(path, "/adbd") == NULL)) {
+        pr_info("hook_manager: unmark %d exec %s\n", current->pid, path);
         ksu_clear_task_tracepoint_flag_if_needed(current);
-	}
+    }
 
     return 0;
 }
