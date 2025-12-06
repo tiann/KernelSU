b65be75dd001a37a114066128923bdf364fa4e77
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-12-06 15:00:52 +0800

    add SupportedKmis command, flatten Debug GetSign command

--- userspace/ksud/src/cli_non_android.rs
+++ userspace/ksud/src/cli_non_android.rs
@@ -20,20 +20,14 @@ enum Commands {
     /// Restore boot or init_boot images patched by KernelSU
     BootRestore(BootRestoreArgs),
 
-    /// For developers
-    Debug {
-        #[command(subcommand)]
-        command: Debug,
-    },
-}
-
-#[derive(clap::Subcommand, Debug)]
-enum Debug {
     /// Get apk size and hash
     GetSign {
         /// apk path
         apk: String,
     },
+
+    /// show supported kmi versions
+    SupportedKmis,
 }
 
 pub fn run() -> Result<()> {
@@ -44,17 +38,23 @@ pub fn run() -> Result<()> {
     log::info!("command: {:?}", cli.command);
 
     let result = match cli.command {
-        Commands::Debug { command } => match command {
-            Debug::GetSign { apk } => {
-                let sign = apk_sign::get_apk_signature(&apk)?;
-                println!("size: {:#x}, hash: {}", sign.0, sign.1);
-                Ok(())
-            }
-        },
+        Commands::GetSign { apk } => {
+            let sign = apk_sign::get_apk_signature(&apk)?;
+            println!("size: {:#x}, hash: {}", sign.0, sign.1);
+            Ok(())
+        }
 
         Commands::BootPatch(boot_patch) => crate::boot_patch::patch(boot_patch),
 
         Commands::BootRestore(boot_restore) => crate::boot_patch::restore(boot_restore),
+
+        Commands::SupportedKmis => {
+            let kmi = crate::assets::list_supported_kmi();
+            for kmi in &kmi {
+                println!("{kmi}");
+            }
+            Ok(())
+        }
     };
 
     if let Err(e) = &result {
