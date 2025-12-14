ff358f2842018f6837fcb0928920ccfe5f03ff38
u9521 <63995396+u9521@users.noreply.github.com>
2025-12-14 22:16:10 +0800

    kernel: refactor and typo fix

--- kernel/su_mount_ns.c
+++ kernel/su_mount_ns.c
@@ -9,7 +9,6 @@
 #include <linux/pid.h>
 #include <linux/sched/task.h>
 #include <linux/slab.h>
-#include <linux/task_work.h>
 #include <linux/syscalls.h>
 #include <linux/task_work.h>
 #include <uapi/linux/mount.h>
@@ -45,121 +44,119 @@ static long ksu_sys_setns(int fd, int flags)
 #endif
 }
 
-static void ksu_setup_mount_ns(int32_t ns_mode)
+// global mode , need CAP_SYS_ADMIN and CAP_SYS_CHROOT to perform setns
+static void ksu_mnt_ns_global(void)
 {
-    const struct cred *old_cred = override_creds(ksu_cred);
-
-    // global mode , need CAP_SYS_ADMIN and CAP_SYS_CHROOT to perform setns
-    if (ns_mode == KSU_NS_GLOBAL) {
-        // save current working directory as absolute path before setns
-        struct path saved_pwd;
-        char pwd_buf[KSU_MAX_PWD_PATH];
-        get_fs_pwd(current->fs, &saved_pwd);
-        char *pwd_path = d_path(&saved_pwd, pwd_buf, sizeof(pwd_buf));
-        if (IS_ERR(pwd_path)) {
-            if (PTR_ERR(pwd_path) == -ENAMETOOLONG) {
-                pr_warn("absolute pwd longer than: %zu\n,skip restore pwd!!",
-                        sizeof(pwd_buf));
-            } else {
-                pr_warn("get absolute pwd failed: %ld\n", PTR_ERR(pwd_path));
-            }
-            pwd_path = NULL;
+    // save current working directory as absolute path before setns
+    struct path saved_pwd;
+    char pwd_buf[KSU_MAX_PWD_PATH];
+    get_fs_pwd(current->fs, &saved_pwd);
+    char *pwd_path = d_path(&saved_pwd, pwd_buf, sizeof(pwd_buf));
+    if (IS_ERR(pwd_path)) {
+        if (PTR_ERR(pwd_path) == -ENAMETOOLONG) {
+            pr_warn("absolute pwd longer than: %zu,skip restore pwd!!\n",
+                    sizeof(pwd_buf));
+        } else {
+            pr_warn("get absolute pwd failed: %ld\n", PTR_ERR(pwd_path));
         }
-        path_put(&saved_pwd);
+        pwd_path = NULL;
+    }
+    path_put(&saved_pwd);
 
-        rcu_read_lock();
-        // &init_task is not init, but swapper/idle, which froks the init process
-        // so we need find init process
-        struct pid *pid_struct = find_pid_ns(1, &init_pid_ns);
-        if (unlikely(!pid_struct)) {
-            rcu_read_unlock();
-            pr_warn("failed to find pid_struct for PID 1\n");
-            goto restore_cred;
-        }
-
-        struct task_struct *pid1_task = get_pid_task(pid_struct, PIDTYPE_PID);
+    rcu_read_lock();
+    // &init_task is not init, but swapper/idle, which forks the init process
+    // so we need find init process
+    struct pid *pid_struct = find_pid_ns(1, &init_pid_ns);
+    if (unlikely(!pid_struct)) {
         rcu_read_unlock();
-        if (unlikely(!pid1_task)) {
-            pr_warn("failed to get task_struct for PID 1\n");
-            goto restore_cred;
-        }
-        struct path ns_path;
-        long ret = ns_get_path(&ns_path, pid1_task, &mntns_operations);
-        put_task_struct(pid1_task);
-        if (ret) {
-            pr_warn("failed get path for init mount namespace: %ld\n", ret);
-            goto restore_cred;
-        }
-        struct file *ns_file = dentry_open(&ns_path, O_RDONLY, ksu_cred);
+        pr_warn("failed to find pid_struct for PID 1\n");
+        return;
+    }
 
-        path_put(&ns_path);
-        if (IS_ERR(ns_file)) {
-            pr_warn("failed open file for init mount namespace: %ld\n",
-                    PTR_ERR(ns_file));
-            goto restore_cred;
-        }
+    struct task_struct *pid1_task = get_pid_task(pid_struct, PIDTYPE_PID);
+    rcu_read_unlock();
+    if (unlikely(!pid1_task)) {
+        pr_warn("failed to get task_struct for PID 1\n");
+        return;
+    }
+    struct path ns_path;
+    long ret = ns_get_path(&ns_path, pid1_task, &mntns_operations);
+    put_task_struct(pid1_task);
+    if (ret) {
+        pr_warn("failed get path for init mount namespace: %ld\n", ret);
+        return;
+    }
+    struct file *ns_file = dentry_open(&ns_path, O_RDONLY, ksu_cred);
 
-        int fd = get_unused_fd_flags(O_CLOEXEC);
-        if (fd < 0) {
-            pr_warn("failed to get an unused fd: %d\n", fd);
-            fput(ns_file);
-            goto restore_cred;
-        }
+    path_put(&ns_path);
+    if (IS_ERR(ns_file)) {
+        pr_warn("failed open file for init mount namespace: %ld\n",
+                PTR_ERR(ns_file));
+        return;
+    }
 
-        fd_install(fd, ns_file);
+    int fd = get_unused_fd_flags(O_CLOEXEC);
+    if (fd < 0) {
+        pr_warn("failed to get an unused fd: %d\n", fd);
+        fput(ns_file);
+        return;
+    }
 
-        ret = ksu_sys_setns(fd, CLONE_NEWNS);
-        if (ret) {
-            pr_warn("call setns failed: %ld\n", ret);
-        }
+    fd_install(fd, ns_file);
+
+    ret = ksu_sys_setns(fd, CLONE_NEWNS);
+    if (ret) {
+        pr_warn("call setns failed: %ld\n", ret);
+        return;
+    }
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 11, 0)
-        ksys_close(fd);
+    ksys_close(fd);
 #else
-        close_fd(fd);
+    close_fd(fd);
 #endif
-        // try to restore working directory using absolute path after setns
-        if (pwd_path) {
-            struct path new_pwd;
-            int err = kern_path(pwd_path, 0, &new_pwd);
-            if (!err) {
-                set_fs_pwd(current->fs, &new_pwd);
-                path_put(&new_pwd);
-            } else {
-                pr_warn("restore pwd failed: %d, path: %s\n", err, pwd_path);
-            }
+    // try to restore working directory using absolute path after setns
+    if (pwd_path) {
+        struct path new_pwd;
+        int err = kern_path(pwd_path, 0, &new_pwd);
+        if (!err) {
+            set_fs_pwd(current->fs, &new_pwd);
+            path_put(&new_pwd);
+        } else {
+            pr_warn("restore pwd failed: %d, path: %s\n", err, pwd_path);
         }
     }
-    // individual mode , need CAP_SYS_ADMIN to perform unshare
-    if (ns_mode == KSU_NS_INDIVIDUAL) {
-        long ret;
-
-        ret = ksys_unshare(CLONE_NEWNS);
-        if (ret) {
-            pr_warn("call ksys_unshare failed: %ld\n", ret);
-            goto restore_cred;
-        }
-
-        // make root mount private
-        struct path root_path;
-        get_fs_root(current->fs, &root_path);
-        int pm_ret;
-        pm_ret = path_mount(NULL, &root_path, NULL, MS_PRIVATE | MS_REC, NULL);
-        path_put(&root_path);
-
-        if (pm_ret < 0) {
-            pr_err("failed to make root private, err: %d\n", pm_ret);
-        }
-    }
-// finally restore cred
-restore_cred:
-    revert_creds(old_cred);
-    return;
 }
 
-void ksu_setup_mount_ns_tw_func(struct callback_head *cb)
+// individual mode , need CAP_SYS_ADMIN to perform unshare and remount
+static void ksu_mnt_ns_individual(void)
+{
+    long ret = ksys_unshare(CLONE_NEWNS);
+    if (ret) {
+        pr_warn("call ksys_unshare failed: %ld\n", ret);
+        return;
+    }
+
+    // make root mount private
+    struct path root_path;
+    get_fs_root(current->fs, &root_path);
+    int pm_ret = path_mount(NULL, &root_path, NULL, MS_PRIVATE | MS_REC, NULL);
+    path_put(&root_path);
+
+    if (pm_ret < 0) {
+        pr_err("failed to make root private, err: %d\n", pm_ret);
+    }
+}
+
+static void ksu_setup_mount_ns_tw_func(struct callback_head *cb)
 {
     struct ksu_mns_tw *tw = container_of(cb, struct ksu_mns_tw, cb);
-    ksu_setup_mount_ns(tw->ns_mode);
+    const struct cred *old_cred = override_creds(ksu_cred);
+    if (tw->ns_mode == KSU_NS_GLOBAL) {
+        ksu_mnt_ns_global();
+    } else {
+        ksu_mnt_ns_individual();
+    }
+    revert_creds(old_cred);
     kfree(tw);
 }
 
@@ -172,8 +169,8 @@ void setup_mount_ns(int32_t ns_mode)
     }
 
     if (ns_mode != KSU_NS_GLOBAL && ns_mode != KSU_NS_INDIVIDUAL) {
-        pr_warn("pid: %d ,unknown mount namespace mode: %d\n", ns_mode,
-                current->pid);
+        pr_warn("pid: %d ,unknown mount namespace mode: %d\n", current->pid,
+                ns_mode);
         return;
     }
 
@@ -189,7 +186,7 @@ void setup_mount_ns(int32_t ns_mode)
     tw->ns_mode = ns_mode;
     if (task_work_add(current, &tw->cb, TWA_RESUME)) {
         kfree(tw);
-        pr_err("add task work faild! skip mnt_ns magic for pid: %d.\n",
+        pr_err("add task work failed! skip mnt_ns magic for pid: %d.\n",
                current->pid);
     }
 }
