0cdeb96613e4e6bcec89962fbda2b36510aa88f9
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2025-12-17 18:17:41 +0800

    kernel: Refactor file_wrapper (#3070)
    - [x] Use d_dname to forge paths: Fixes an issue where some programs
    (such as
    [screen](https://cgit.git.savannah.gnu.org/cgit/screen.git/tree/src/screen.c?id=27b44ea4ebb18296e6ceffaf5a2e3638c8a950be#n1914))
    required a valid tty path.
    - [x] Enhance security using anon_inode_create_getfile and implements
    this function for older kernel versions.
    - [x] Support reopen from /proc/pid/fd

--- kernel/file_wrapper.h
+++ kernel/file_wrapper.h
@@ -4,11 +4,7 @@
 #include <linux/file.h>
 #include <linux/fs.h>
 
-struct ksu_file_wrapper {
-    struct file *orig;
-    struct file_operations ops;
-};
+int ksu_install_file_wrapper(int fd);
+void ksu_file_wrapper_init(void);
 
-struct ksu_file_wrapper *ksu_create_file_wrapper(struct file *fp);
-void ksu_delete_file_wrapper(struct ksu_file_wrapper *data);
 #endif // KSU_FILE_WRAPPER_H
