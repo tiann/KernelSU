641406111c38e2cb4a6e31fe78949a44e315d380
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2025-12-22 18:33:17 +0300

    ksuinit: update dependencies and migrate to 2024 (#3088)
    `goblin` version is too old, i recommend to upstream it since it's fork.
    because of it, `scroll` cannot be updated.
    
    Edits welcomed.

--- userspace/ksuinit/src/loader.rs
+++ userspace/ksuinit/src/loader.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
-use goblin::elf::{section_header, sym::Sym, Elf};
+use goblin::elf::{Elf, section_header, sym::Sym};
 use rustix::{cstr, system::init_module};
-use scroll::{ctx::SizeWith, Pwrite};
+use scroll::{Pwrite, ctx::SizeWith};
 use std::collections::HashMap;
 use std::fs;
 
