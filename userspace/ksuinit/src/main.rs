5ae9fe336d85384cc3af559bb53f9e521df3d08e
Wang Han <416810799@qq.com>
2025-12-22 21:49:35 +0800

    Refactor main function to use unsafe block

--- userspace/ksuinit/src/main.rs
+++ userspace/ksuinit/src/main.rs
@@ -10,10 +10,8 @@ use rustix::{cstr, runtime::execve};
 /// https://github.com/rust-lang/rust/blob/3071aefdb2821439e2e6f592f41a4d28e40c1e79/library/std/src/sys/unix/mod.rs#L80
 /// So we use the C main function and call rust code from there
 #[unsafe(no_mangle)]
-pub extern "C" fn main(_argc: i32, argv: *const *const u8, envp: *const *const u8) -> i32 {
+pub unsafe extern "C" fn main(_argc: i32, argv: *const *const u8, envp: *const *const u8) -> i32 {
     let _ = init::init();
-    unsafe {
-        execve(cstr!("/init"), argv, envp);
-    }
+    execve(cstr!("/init"), argv, envp);
     0
 }
