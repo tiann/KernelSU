2ba64dc600a96acaa05b4e3a7e4171f4ca724db2
u9521 <63995396+u9521@users.noreply.github.com>
2026-02-02 22:03:05 +0800

    manager: Allow patching for devices with other different KMI versions (#3185)
    Allow manager to patch images for devices with other GKI kernels in
    different KMI version
    
    ---------
    
    Signed-off-by: u9521 <63995396+u9521@users.noreply.github.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/component/ChooseKmiDialog.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/component/ChooseKmiDialog.kt
@@ -1,74 +1,98 @@
 package me.weishu.kernelsu.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import me.weishu.kernelsu.R
+import me.weishu.kernelsu.ui.util.getCurrentKmi
 import me.weishu.kernelsu.ui.util.getSupportedKmis
-import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.basic.ButtonDefaults
 import top.yukonga.miuix.kmp.basic.TextButton
-import top.yukonga.miuix.kmp.extra.SuperArrow
+import top.yukonga.miuix.kmp.extra.CheckboxLocation
+import top.yukonga.miuix.kmp.extra.SuperCheckbox
 import top.yukonga.miuix.kmp.extra.SuperDialog
-import top.yukonga.miuix.kmp.theme.MiuixTheme
-import top.yukonga.miuix.kmp.theme.MiuixTheme.colorScheme
 
 @Composable
 fun ChooseKmiDialog(
     showDialog: MutableState<Boolean>,
     onSelected: (String?) -> Unit
 ) {
-    val supportedKmi by produceState(initialValue = emptyList()) {
+    val supportedKMIs by produceState(initialValue = emptyList()) {
         value = getSupportedKmis()
     }
-    val options = supportedKmi.map { it }
-
+    val currentKmi by produceState(initialValue = "") {
+        value = getCurrentKmi()
+    }
+    val currentSelection = rememberSaveable(currentKmi) { mutableStateOf(currentKmi) }
     SuperDialog(
         show = showDialog,
-        insideMargin = DpSize(0.dp, 0.dp),
+        title = stringResource(R.string.select_kmi),
+        summary = stringResource(R.string.current_kmi, currentKmi.let { it.ifBlank { "Unknown" } }),
+        insideMargin = DpSize(0.dp, 24.dp),
         onDismissRequest = {
             showDialog.value = false
+            currentSelection.value = currentKmi
         },
-        content = {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 24.dp, bottom = 12.dp),
-                text = stringResource(R.string.select_kmi),
-                fontSize = MiuixTheme.textStyles.title4.fontSize,
-                fontWeight = FontWeight.Medium,
-                textAlign = TextAlign.Center,
-                color = colorScheme.onSurface
-            )
-            options.forEachIndexed { index, type ->
-                SuperArrow(
-                    title = type,
+    ) {
+        Column(modifier = Modifier.heightIn(max = 500.dp)) {
+            LazyColumn(modifier = Modifier.weight(1f, fill = false)) {
+                items(supportedKMIs) { kmi ->
+                    SuperCheckbox(
+                        title = kmi,
+                        summary = if (kmi == currentKmi) stringResource(R.string.current_device_kmi) else null,
+                        insideMargin = PaddingValues(horizontal = 30.dp, vertical = 16.dp),
+                        checkboxLocation = CheckboxLocation.End,
+                        checked = currentSelection.value == kmi,
+                        holdDownState = currentSelection.value == kmi,
+                        onCheckedChange = { _ ->
+                            currentSelection.value = kmi
+                        }
+                    )
+                }
+            }
+            Spacer(Modifier.height(12.dp))
+            Row(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                TextButton(
                     onClick = {
-                        onSelected(type)
+                        showDialog.value = false
+                        currentSelection.value = currentKmi
+                    },
+                    text = stringResource(android.R.string.cancel),
+                    modifier = Modifier.weight(1f),
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                TextButton(
+                    enabled = supportedKMIs.contains(currentSelection.value),
+                    onClick = {
+                        onSelected(currentSelection.value)
                         showDialog.value = false
                     },
-                    insideMargin = PaddingValues(horizontal = 24.dp, vertical = 12.dp)
+                    text = stringResource(R.string.confirm),
+                    modifier = Modifier.weight(1f),
+                    colors = ButtonDefaults.textButtonColorsPrimary()
                 )
             }
-            TextButton(
-                text = stringResource(id = android.R.string.cancel),
-                onClick = {
-                    showDialog.value = false
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 12.dp, bottom = 24.dp)
-                    .padding(horizontal = 24.dp)
-            )
         }
-    )
+    }
 }
