323df9a778e7096552c20642e291666d9494d183
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2026-03-12 14:32:01 +0800

    manager: bump MINIMAL_SUPPORTED_KERNEL to 32336

--- manager/app/src/main/java/me/weishu/kernelsu/Natives.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/Natives.kt
@@ -19,7 +19,8 @@ object Natives {
     // 11071: Fix the issue of failing to set a custom SELinux type.
     // 12143: breaking: new supercall impl
     // 32310: new get_allow_list ioctl
-    const val MINIMAL_SUPPORTED_KERNEL = 32310
+    // 32336: new set_sepolicy ioctl
+    const val MINIMAL_SUPPORTED_KERNEL = 32336
 
     const val KERNEL_SU_DOMAIN = "u:r:su:s0"
 
