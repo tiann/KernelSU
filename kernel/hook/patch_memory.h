73fc1003975be66f5e7dc73df3e177d9da650870
AlexLiuDev233 <wzylin11@outlook.com>
2026-03-27 14:02:03 +0000

    kernel: fix compile for x86-64 linux kernel v6.13- (#3351)

--- kernel/hook/patch_memory.h
+++ kernel/hook/patch_memory.h
@@ -9,12 +9,18 @@
 #include <linux/types.h>
 #include "linux/version.h"
 
+#ifdef __aarch64__
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
 #include "asm/text-patching.h" // IWYU pragma: keep
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 14, 0)
 #include "asm/patching.h" // IWYU pragma: keep
 #else
 #include "asm/insn.h" // IWYU pragma: keep
+#endif
+#elif __x86_64__
+#include "asm/text-patching.h" // IWYU pragma: keep
+#else
+#error "Unsupported arch"
 #endif
 
 #define KSU_PATCH_TEXT_FLUSH_DCACHE 1
