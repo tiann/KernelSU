7e54528af3c424ed6bfd13f80d60660d5af003c5
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-03-31 12:41:09 +0800

    ksud: remove ksud before update it
    - Since ksud sulogd is a long-running daemon, write operations to the ksud file will be rejected, making it impossible to update the file. Therefore, we need to recreate it.

--- userspace/ksud/src/utils.rs
+++ userspace/ksud/src/utils.rs
@@ -191,6 +191,7 @@ fn link_ksud_to_bin() -> Result<()> {
 
 pub fn install(magiskboot: Option<PathBuf>) -> Result<()> {
     ensure_dir_exists(defs::ADB_DIR)?;
+    let _ = std::fs::remove_file(defs::DAEMON_PATH);
     std::fs::copy(
         std::env::current_exe().with_context(|| "Failed to get self exe path")?,
         defs::DAEMON_PATH,
