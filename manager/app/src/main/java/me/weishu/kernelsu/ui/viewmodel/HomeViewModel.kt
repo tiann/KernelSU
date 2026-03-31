33de2cc6b4e44034289f3029961eaa32a5e603e3
Whoami <git@im.style>
2026-03-31 16:31:34 +0800

    manager: remove getSeccompStatus method and update seccomp status retrieval

--- manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/HomeViewModel.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/HomeViewModel.kt
@@ -72,7 +72,9 @@ class HomeViewModel : ViewModel() {
                 managerVersion = "${managerVersion.versionName} (${managerVersion.versionCode})",
                 fingerprint = Build.FINGERPRINT,
                 selinuxStatus = getSELinuxStatusRaw(),
-                seccompStatus = Natives.getSeccompStatus(),
+                seccompStatus = runCatching {
+                    Os.prctl(21 /* PR_GET_SECCOMP */, 0, 0, 0, 0)
+                }.getOrDefault(-1),
             ),
         )
     }
