920265a3599e8c7610699f7829f3259380513c4c
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-04-01 21:32:17 +0800

    setcurrent to adbd when unroot

--- manager/app/src/main/cpp/adbroot.cc
+++ manager/app/src/main/cpp/adbroot.cc
@@ -1,6 +1,7 @@
 #include <string>
 #include <dlfcn.h>
 #include <cstdlib>
+#include <unistd.h>
 
 // Skip drop privileges
 // https://cs.android.com/android/platform/superproject/+/android-latest-release:packages/modules/adb/daemon/main.cpp;l=123;drc=3b74954ec50836e0c8faeed1877fefb1dc2de006
@@ -19,6 +20,13 @@ int __android_log_is_debuggable() {
 std::string GetProperty(const std::string &key, const std::string &default_value)
 asm ("_ZN7android4base11GetPropertyERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEES9_");
 
+// skip setcon
+extern "C"
+[[gnu::visibility("default"), gnu::used]]
+int selinux_android_setcon(const char *con) {
+    return 0;
+}
+
 // Enable adb root by default, without run `adb root` command explicitly or setprop
 // https://cs.android.com/android/platform/superproject/+/android-latest-release:packages/modules/adb/daemon/main.cpp;l=84;drc=3b74954ec50836e0c8faeed1877fefb1dc2de006
 [[gnu::visibility("default"), gnu::used]]
@@ -28,17 +36,18 @@ std::string GetProperty(const std::string &key, const std::string &default_value
 
     if (key == "service.adb.root") {
         // allow drop privilege by prop, but don't drop by default
-        return orig_fn(key, "1");
+        auto res = orig_fn(key, "1");
+        if (res == "0") {
+            static constexpr const auto kSetconSymbol = "selinux_android_setcon";
+            static auto orig_setcon = (decltype(selinux_android_setcon) *) dlsym(RTLD_NEXT, kSetconSymbol);
+
+            // If we are requested to unroot, setcurrent to adbd
+            orig_setcon("u:r:adbd:s0");
+        }
+        return res;
     }
 
     return orig_fn(key, default_value);
-}
-
-// skip setcon
-extern "C"
-[[gnu::visibility("default"), gnu::used]]
-int selinux_android_setcon(const char *con) {
-    return 0;
 }
 
 [[gnu::used, gnu::constructor(0)]]
