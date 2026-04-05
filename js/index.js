53eb87c212c3646808e63b636767a2618064f9c9
KOWX712 <leecc0503@gmail.com>
2026-04-05 19:06:28 +0800

    js: add file interface

--- js/index.js
+++ js/index.js
@@ -144,3 +144,63 @@ export function getPackagesInfo(packages) {
 export function exit() {
   ksu.exit();
 }
+
+export const io = {
+  File(path) {
+    return ksu.io().File(path);
+  },
+
+  FileInputStream() {
+    const impl = ksu.io().FileInputStream();
+    return {
+      open(path) { return impl.open(path); },
+      read(id, maxBytes) { return impl.read(id, maxBytes); },
+      available(id) { return impl.available(id); },
+      close(id) { return impl.close(id); },
+    };
+  },
+
+  FileOutputStream() {
+    const impl = ksu.io().FileOutputStream();
+    return {
+      open(path, append) { return impl.open(path, append); },
+      write(id, data) { return impl.write(id, data); },
+      writeByte(id, b) { return impl.writeByte(id, b); },
+      flush(id) { return impl.flush(id); },
+      close(id) { return impl.close(id); },
+    };
+  },
+
+  RandomAccessFile() {
+    const impl = ksu.io().RandomAccessFile();
+    return {
+      open(path, mode) { return impl.open(path, mode); },
+      read(id) { return impl.read(id); },
+      readBytes(id, len) { return impl.readBytes(id, len); },
+      readBoolean(id) { return impl.readBoolean(id); },
+      readByte(id) { return impl.readByte(id); },
+      readInt(id) { return impl.readInt(id); },
+      readLong(id) { return impl.readLong(id); },
+      readShort(id) { return impl.readShort(id); },
+      readFloat(id) { return impl.readFloat(id); },
+      readDouble(id) { return impl.readDouble(id); },
+      readUTF(id) { return impl.readUTF(id); },
+      readLine(id) { return impl.readLine(id); },
+      write(id, b) { impl.write(id, b); },
+      writeBase64(id, data) { impl.writeBase64(id, data); },
+      writeBoolean(id, v) { impl.writeBoolean(id, v); },
+      writeByte(id, v) { impl.writeByte(id, v); },
+      writeInt(id, v) { impl.writeInt(id, v); },
+      writeLong(id, v) { impl.writeLong(id, v); },
+      writeShort(id, v) { impl.writeShort(id, v); },
+      writeFloat(id, v) { impl.writeFloat(id, v); },
+      writeDouble(id, v) { impl.writeDouble(id, v); },
+      writeUTF(id, str) { impl.writeUTF(id, str); },
+      seek(id, pos) { return impl.seek(id, pos); },
+      getFilePointer(id) { return impl.getFilePointer(id); },
+      length(id) { return impl.length(id); },
+      setLength(id, newLength) { return impl.setLength(id, newLength); },
+      close(id) { return impl.close(id); },
+    };
+  },
+};
