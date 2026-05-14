b228ffeb096d74ff798d1a0744f12f642ef4d0cf
XiaoTong6666 <3278671549@qq.com>
2026-05-14 16:24:48 +0800

    simplify symbol lookup and prioritize CFI jump tables

--- kernel/infra/symbol_resolver.c
+++ kernel/infra/symbol_resolver.c
@@ -5,11 +5,13 @@
 
 #include "infra/symbol_resolver.h"
 
+static const char cfi_suffix[] = ".cfi_jt";
+static const size_t cfi_suffix_len = sizeof(cfi_suffix) - 1;
+
 struct ksu_lookup_symbol_ctx {
     const char *symbol_name;
     size_t symbol_len;
     void *match;
-    void *cfi_match;
 };
 
 static bool ksu_symbol_has_suffix(const char *name, size_t name_len, const char *suffix, size_t suffix_len)
@@ -20,8 +22,6 @@ static bool ksu_symbol_has_suffix(const char *name, size_t name_len, const char 
 static int ksu_lookup_symbol_handle_match(void *data, const char *name, unsigned long addr)
 {
     struct ksu_lookup_symbol_ctx *ctx = data;
-    static const char cfi_suffix[] = ".cfi_jt";
-    const size_t cfi_suffix_len = sizeof(cfi_suffix) - 1;
     size_t name_len;
 
     if (!name || !addr)
@@ -36,7 +36,7 @@ static int ksu_lookup_symbol_handle_match(void *data, const char *name, unsigned
     }
 
     if (ksu_symbol_has_suffix(name, name_len, cfi_suffix, cfi_suffix_len)) {
-        ctx->cfi_match = (void *)addr;
+        ctx->match = (void *)addr;
         return 1;
     }
 
@@ -66,30 +66,12 @@ void *ksu_lookup_symbol(const char *symbol_name)
     struct ksu_lookup_symbol_ctx ctx = {
         .symbol_name = symbol_name,
     };
-    char cfi_name[KSYM_NAME_LEN];
-    void *addr;
-    static const char cfi_suffix[] = ".cfi_jt";
-    const size_t cfi_suffix_len = sizeof(cfi_suffix) - 1;
 
     if (!symbol_name || !symbol_name[0])
         return NULL;
 
     ctx.symbol_len = strlen(symbol_name);
 
-    if (!ksu_symbol_has_suffix(symbol_name, ctx.symbol_len, cfi_suffix, cfi_suffix_len) &&
-        ctx.symbol_len + cfi_suffix_len + 1 <= sizeof(cfi_name) &&
-        strscpy(cfi_name, symbol_name, sizeof(cfi_name)) > 0) {
-        strlcat(cfi_name, cfi_suffix, sizeof(cfi_name));
-        addr = (void *)kallsyms_lookup_name(cfi_name);
-        if (addr)
-            return addr;
-    }
-
     kallsyms_on_each_symbol(ksu_lookup_symbol_cb, &ctx);
-    if (ctx.cfi_match)
-        return ctx.cfi_match;
-    if (ctx.match)
-        return ctx.match;
-
-    return (void *)kallsyms_lookup_name(symbol_name);
+    return ctx.match;
 }
