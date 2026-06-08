hworr/xx_ksu

kernel, ksud, manager: implement uapi version check
We always have an problem:
We should keep forward compatibility in uapi, or users may face problem

e.g:
new sepolicy api (https://github.com/tiann/KernelSU/commit/b98c05eb129ec923dafef2c219b5a38c0f0ecb20)
new allowlist api (https://github.com/tiann/KernelSU/commit/9f68f239f62c242964311aeec3080ced9cda4d29)

They all resulted in varying degrees of functional impairment and severely impacted the user experience.

For example
new sepolicy api cause some modules looks "working", but actually not, them sepolicy.rule haven't be loaded.
new allowlist api cause manager show "0 SuperUsers"

That's cause by userspace version mismatch with kernel version

This pull request implement an uapi check, when userspace see uapi version mismatch with kernel's uapi version
ksud will stop handle post_fs_data, services, boot_completed and more
manager will show "incompatible kernel" warning card

This gives users a clear indication: they messed up,
either using an older version of the manager with the new kernel
or trying to make the new manager work with an older kernel.

To prevent users from sending invalid bug reports to the module developer/us due to their own issues.

Signed-off-by: AlexLiuDev233 <wzylin11@outlook.com>
Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

manager/app/src/main/cpp/jni.cc
@@ -24,6 +24,12 @@ Java_me_weishu_kernelsu_Natives_getVersion(JNIEnv *env, jobject) {
 }
 
 extern "C"
+JNIEXPORT jboolean JNICALL
+Java_me_weishu_kernelsu_Natives_checkUAPIMismatch(JNIEnv *env, jobject) {
+    return is_uapi_version_mismatch();
+}
+
+extern "C"
 JNIEXPORT jint JNICALL
 Java_me_weishu_kernelsu_Natives_getSuperuserCount(JNIEnv *env, jobject) {
     struct ksu_new_get_allow_list_cmd cmd = {

tiann/KernelSU

manager/app/src/main/cpp/jni.cc
@@ -25,6 +25,18 @@ Java_me_weishu_kernelsu_Natives_getVersion(JNIEnv *env, jobject) {
 
 extern "C"
 JNIEXPORT jint JNICALL
+Java_me_weishu_kernelsu_Natives_getKernelUAPIVersion(JNIEnv *env, jobject) {
+    return get_kernel_uapi_version();
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_me_weishu_kernelsu_Natives_getManagerUAPIVersion(JNIEnv *env, jobject) {
+    return get_manager_uapi_version();
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
 Java_me_weishu_kernelsu_Natives_getSuperuserCount(JNIEnv *env, jobject) {
     struct ksu_new_get_allow_list_cmd cmd = {
         .count = 0