4a92049e44cb4ad3ec50c889e3259bfa5d685b01
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2026-08-20 22:26:06 +0800

    fix(kernel): adapt to backport of 6.6 kernel, fix #3629 (#3632)

--- kernel/core/init.c
+++ kernel/core/init.c
@@ -136,6 +136,7 @@ int __init kernelsu_init(void)
     ksu_selinux_hide_init();
 
     ksu_supercalls_init();
+    ksu_app_profile_init();
 
     if (ksu_late_loaded) {
         pr_info("late load mode, skipping kprobe hooks\n");
--- kernel/policy/app_profile.h
+++ kernel/policy/app_profile.h
@@ -2,6 +2,7 @@
 #define __KSU_H_APP_PROFILE
 
 #include "uapi/app_profile.h"
+#include "linux/init.h"
 
 #define TIF_KSU_DISABLE_ESCAPE_WITH_ROOT 63
 
@@ -9,5 +10,7 @@
 int escape_with_root_profile(void);
 
 void escape_to_root_for_init(void);
+
+void __init ksu_app_profile_init(void);
 
 #endif
