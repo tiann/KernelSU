3d6562e5f79d0a42c8965f7cb4a59553e0aea62e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-09-21 12:11:47 +0800

    kernel: util: wire up ksyscall
    k5.10 / 5.15 will be eol this december.
    people will be backporting stuff to it. lets try to be more portable starting with these
    
    related:
    https://review.lineageos.org/c/LineageOS/android_kernel_qcom_sm8450/+/471855
    https://github.com/LineageOS/android_kernel_qcom_sm8450/commit/f525903dd56a63965654396eb467550970c444aa
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -9,11 +9,51 @@
 #include <linux/err.h>
 #include <linux/cred.h>
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
-#define ksu_close_fd close_fd
-#else
-#define ksu_close_fd ksys_close
+#if defined(__aarch64__)
+#define KSU_SYS_PREFIX(name) __arm64_sys_##name
+#elif defined(__x86_64__)
+#define KSU_SYS_PREFIX(name) __x64_sys_##name
+#else // wire up your arch here.
+static_assert(1 == 0, "Unsupported architecture!");
+#define KSU_SYS_PREFIX(name) sys_##name
 #endif
+
+/**
+ * ksyscall: call syscalls from kernelspace
+ * - tries to copy unistd's syscall()
+ *
+ * usage: ksyscall(close, fd);
+ */
+#define __ksyscall(name, a, b, c, d, e, f)                                                                             \
+    ({                                                                                                                 \
+        extern long KSU_SYS_PREFIX(name)(const struct pt_regs *);                                                      \
+        struct pt_regs regs = { 0 };                                                                                   \
+        PT_REGS_PARM1(&regs) = (unsigned long)(a);                                                                     \
+        PT_REGS_PARM2(&regs) = (unsigned long)(b);                                                                     \
+        PT_REGS_PARM3(&regs) = (unsigned long)(c);                                                                     \
+        PT_REGS_SYSCALL_PARM4(&regs) = (unsigned long)(d);                                                             \
+        PT_REGS_PARM5(&regs) = (unsigned long)(e);                                                                     \
+        PT_REGS_PARM6(&regs) = (unsigned long)(f);                                                                     \
+        (long)KSU_SYS_PREFIX(name)(&regs);                                                                             \
+    })
+
+// https://elixir.bootlin.com/musl/v1.2.6/source/src/internal/syscall.h#L45
+#define ksyscall_0(name) __ksyscall(name, 0, 0, 0, 0, 0, 0)
+#define ksyscall_1(name, a) __ksyscall(name, a, 0, 0, 0, 0, 0)
+#define ksyscall_2(name, a, b) __ksyscall(name, a, b, 0, 0, 0, 0)
+#define ksyscall_3(name, a, b, c) __ksyscall(name, a, b, c, 0, 0, 0)
+#define ksyscall_4(name, a, b, c, d) __ksyscall(name, a, b, c, d, 0, 0)
+#define ksyscall_5(name, a, b, c, d, e) __ksyscall(name, a, b, c, d, e, 0)
+#define ksyscall_6(name, a, b, c, d, e, f) __ksyscall(name, a, b, c, d, e, f)
+
+#define __ksyscall_arg_n(_1, _2, _3, _4, _5, _6, _7, N, ...) N
+#define __ksyscall_count_args(...) __ksyscall_arg_n(__VA_ARGS__, 6, 5, 4, 3, 2, 1, 0)
+#define __ksyscall_concat(a, b) a##b
+#define __ksyscall_exp(func, arg) __ksyscall_concat(func, arg)
+#define ksyscall(...) __ksyscall_exp(ksyscall_, __ksyscall_count_args(__VA_ARGS__))(__VA_ARGS__)
+
+#define ksu_close_fd(fd) ({ ksyscall(close, fd); })
+#define ksu_sys_setns(fd, flags) ({ ksyscall(setns, fd, flags); })
 
 static inline struct file *ksu_filp_open_nonotify(const char *path, int flags)
 {
